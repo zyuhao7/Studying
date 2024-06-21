508c767f1ad67516961a14084bcc6f1d1ac39d6f
徐浩 <117811771+zyuhao7@users.noreply.github.com>
2024-06-21 09:32:28 +0800

    基于锁实现 哈希表和单链表

--- C++Books/C++并发编程实战/第六章-设计基于锁的并发数据结构/BaseOnLockConDataStruct.cc
+++ C++Books/C++并发编程实战/第六章-设计基于锁的并发数据结构/BaseOnLockConDataStruct.cc
@@ -2,7 +2,11 @@
 #include <iostream>
 #include <mutex>
 #include <condition_variable>
+#include <memory>
 #include <queue>
+#include <shared_mutex>
+#include <list>
+#include <hashtable.h>
 using namespace std;
 
 #if 0
@@ -301,7 +305,475 @@ public:
 };
 #endif
 
+#if 0
+
+template <typename T>
+class threadsafe_queue
+{
+private:
+    struct node
+    {
+        std::shared_ptr<T> data;
+        std::unique_ptr<node> next;
+    };
+    std::mutex head_mutex;
+    std::mutex tail_mutex;
+
+    std::unique_ptr<node> head;
+    node *tail;
+
+    node *get_tail()
+    {
+        std::lock_guard<std::mutex> tail_lock(tail_mutex);
+        return tail;
+    }
+
+    std::unique_ptr<node> pop_head()
+    {
+        std::lock_guard<std::mutex> head_lock(head_mutex);
+        if (head.get() == get_tail())
+        {
+            return nullptr;
+        }
+        std::unique_ptr<node> old_head = std::move(head);
+        head = std::move(old_head->next);
+        return old_head;
+    }
+
+public:
+    threadsafe_queue()
+        : head(new node), tail(head.get())
+    {}
+    threadsafe_queue(const threadsafe_queue &other) = delete;
+    threadsafe_queue &operator=(const threadsafe_queue &other) = delete;
+
+    std::shared_ptr<T> try_pop()
+    {
+        std::unique_ptr<node> old_head = pop_head();
+        return old_head ? old_head->data : std::shared_ptr<T>();
+    }
+
+    void push(T new_value)
+    {
+        std::shared_ptr<T> new_data(
+            std::make_shared<T>(std::move(new_value)));
+        std::unique_ptr<node> p(new node);
+        node *const new_tail = p.get();
+        std::lock_guard<std::mutex> tail_lock(tail_mutex);
+        tail->data = new_data;
+        tail->next = std::move(p);
+        tail = new_tail;
+    }
+};
+
+#endif
+
+#if 0
+template <typename T>
+class threadsafe_queue
+{
+private:
+    struct node
+    {
+        std::shared_ptr<T> data;
+        std::unique_ptr<node> next;
+    };
+
+    std::mutex head_mutex;
+    std::mutex tail_mutex;
+
+    std::unique_ptr<node> head;
+    node *tail;
+    std::condition_variable data_cond;
+
+public:
+    threadsafe_queue()
+        : head(new node), tail(head.get())
+    {
+    }
+    threadsafe_queue(const threadsafe_queue &other) = delete;
+    threadsafe_queue &operator=(const threadsafe_queue &other) = delete;
+
+    std::shared_ptr<T> try_pop();
+    bool try_pop(T &value);
+    std::shared_ptr<T> wait_and_pop();
+    void wait_and_pop(T &value);
+    void push(T new_value);
+    bool empty();
+};
+
+template <typename T>
+void threadsafe_queue<T>::push(T new_value)
+{
+    std::shared_ptr<T> new_data(
+        std::make_shared<T>(std::move(new_value)));
+    std::unique_ptr<node> p(new node);
+    {
+        std::lock_guard<std::mutex> tail_lock(tail_mutex);
+        tail->data = new_data;
+        node *const new_tail = p.get();
+        tail->next = std::move(p);
+        tail = new_tail;
+    }
+    data_cond.notify_one();
+}
+#endif
+
+#if 0
+template <typename T>
+class threadsafe_queue
+{
+private:
+    struct node
+    {
+        std::shared_ptr<T> data;
+        std::unique_ptr<node> next;
+    };
+
+    std::mutex head_mutex;
+    std::mutex tail_mutex;
+
+    std::unique_ptr<node> head;
+    node *tail;
+    std::condition_variable data_cond;
+
+private:
+    node *get_tail()
+    {
+        std::lock_guard<std::mutex> tail_lock(tail_mutex);
+        return tail;
+    }
+
+    std::unique_ptr<node> pop_head() // 1
+    {
+        std::unique_ptr<node> old_head = std::move(head);
+        head = std::move(old_head->next);
+        return old_head;
+    }
+
+    std::unique_lock<std::mutex> wait_for_data() // 2
+    {
+        std::unique_lock<std::mutex> head_lock(head_mutex);
+        data_cond.wait(head_lock, [&]
+                       { return head.get() != get_tail(); });
+        return std::move(head_lock); // 3
+    }
+
+    std::unique_ptr<node> wait_pop_head()
+    {
+        std::unique_lock<std::mutex> head_lock(wait_for_data()); // 4
+        return pop_head();
+    }
+
+    std::unique_ptr<node> wait_pop_head(T &value)
+    {
+        std::unique_lock<std::mutex> head_lock(wait_for_data()); // 5
+        value = std::move(*head->data);
+        return pop_head();
+    }
+
+public:
+    std::shared_ptr<T> wait_and_pop()
+    {
+        std::unique_ptr<node> const old_head = wait_pop_head();
+        return old_head->data;
+    }
+
+    void wait_and_pop(T &value)
+    {
+        std::unique_ptr<node> const old_head = wait_pop_head(value);
+    }
+};
+
+#endif
+
+#if 0
+template <typename T>
+class threadsafe_queue
+{
+private:
+    struct node
+    {
+        std::shared_ptr<T> data;
+        std::unique_ptr<node> next;
+    };
+
+    std::mutex head_mutex;
+    std::mutex tail_mutex;
+
+    std::unique_ptr<node> head;
+    node *tail;
+    std::condition_variable data_cond;
+
+private:
+    std::unique_ptr<node> try_pop_head()
+    {
+        std::lock_guard<std::mutex> head_lock(head_mutex);
+        if (head.get() == get_tail())
+        {
+            return std::unique_ptr<node>();
+        }
+        return pop_head();
+    }
+
+    std::unique_ptr<node> try_pop_head(T &value)
+    {
+        std::lock_guard<std::mutex> head_lock(head_mutex);
+        if (head.get() == get_tail())
+        {
+            return std::unique_ptr<node>();
+        }
+        value = std::move(*head->data);
+        return pop_head();
+    }
+
+public:
+    std::shared_ptr<T> try_pop()
+    {
+        std::unique_ptr<node> old_head = try_pop_head();
+        return old_head ? old_head->data : std::shared_ptr<T>();
+    }
+
+    bool try_pop(T &value)
+    {
+        std::unique_ptr<node> const old_head = try_pop_head(value);
+        return old_head;
+    }
+
+    bool empty()
+    {
+        std::lock_guard<std::mutex> head_lock(head_mutex);
+        return (head.get() == get_tail());
+    }
+};
+
+#endif
+
+#if 0
+template <typename Key, typename Value, typename Hash = std::hash<Key>>
+class threadsafe_lookup_table
+{
+private:
+    class bucket_type
+    {
+    private:
+        typedef std::pair<Key, Value> bucket_value;
+        typedef std::list<bucket_value> bucket_data;
+        typedef typename bucket_data::iterator bucket_iterator;
+
+        bucket_data data;
+        mutable std::shared_mutex mutex; // 1
+
+        bucket_iterator find_entry_for(Key const &key) const // 2
+        {
+            return std::find_if(data.begin(), data.end(),
+                                [&](bucket_value const &item)
+                                { return item.first == key; });
+        }
+
+    public:
+        Value value_for(Key const &key, Value const &default_value) const
+        {
+            std::shared_lock<std::shared_mutex> lock(mutex); // 3
+            bucket_iterator const found_entry = find_entry_for(key);
+            return (found_entry == data.end()) ? default_value : found_entry->second;
+        }
+
+        void add_or_update_mapping(Key const &key, Value const &value)
+        {
+            std::unique_lock<std::shared_mutex> lock(mutex); // 4
+            bucket_iterator const found_entry = find_entry_for(key);
+            if (found_entry == data.end())
+            {
+                data.push_back(bucket_value(key, value));
+            }
+            else
+            {
+                found_entry->second = value;
+            }
+        }
+
+        void remove_mapping(Key const &key)
+        {
+            std::unique_lock<std::shared_mutex> lock(mutex); // 5
+            bucket_iterator const found_entry = find_entry_for(key);
+            if (found_entry != data.end())
+            {
+                data.erase(found_entry);
+            }
+        }
+    };
+
+    std::vector<std::unique_ptr<bucket_type>> buckets; // 6
+    Hash hasher;
+
+    bucket_type &get_bucket(Key const &key) const // 7
+    {
+        std::size_t const bucket_index = hasher(key) % buckets.size();
+        return *buckets[bucket_index];
+    }
+
+public:
+    typedef Key key_type;
+    typedef Value mapped_type;
+
+    typedef Hash hash_type;
+    threadsafe_lookup_table(unsigned num_buckets = 19, Hash const &hasher_ = Hash())
+        : buckets(num_buckets), hasher(hasher_)
+    {
+        for (unsigned i = 0; i < num_buckets; ++i)
+        {
+            buckets[i].reset(new bucket_type);
+        }
+    }
+
+    threadsafe_lookup_table(threadsafe_lookup_table const &other) = delete;
+    threadsafe_lookup_table &operator=(threadsafe_lookup_table const &other) = delete;
+
+    Value value_for(Key const &key,
+                    Value const &default_value = Value()) const
+    {
+        return get_bucket(key).value_for(key, default_value); // 8
+    }
+
+    void add_or_update_mapping(Key const &key, Value const &value)
+    {
+        get_bucket(key).add_or_update_mapping(key, value); // 9
+    }
+
+    void remove_mapping(Key const &key)
+    {
+        get_bucket(key).remove_mapping(key); // 10  }
+    };
+};
+std::map<Key, Value> threadsafe_lookup_table::get_map() const
+{
+    std::vector<std::unique_lock<std::shared_mutex>> locks;
+    for (unsigned i = 0; i < buckets.size(); ++i)
+    {
+        locks.push_back(
+            std::unique_lock<std::shared_mutex>(buckets[i].mutex));
+    }
+    std::map<Key, Value> res;
+    for (unsigned i = 0; i < buckets.size(); ++i)
+    {
+        for (bucket_iterator it = buckets[i].data.begin();
+             it != buckets[i].data.end();
+             ++it)
+        {
+            res.insert(*it);
+        }
+    }
+    return res;
+}
+#endif
+
+#if 0
+template <typename T>
+class threadsafe_list
+{
+    struct node // 1
+    {
+        std::mutex m;
+        std::shared_ptr<T> data;
+        std::unique_ptr<node> next;
+        node()
+            : next() //  2
+        {
+        }
+
+        node(T const &value) // 3
+            : data(std::make_shared<T>(value))
+        {
+        }
+    };
+
+    node head;
+
+public:
+    threadsafe_list()
+    {
+    }
+
+    ~threadsafe_list()
+    {
+        remove_if([](node const &)
+                  { return true; });
+    }
+
+    threadsafe_list(threadsafe_list const &other) = delete;
+    threadsafe_list &operator=(threadsafe_list const &other) = delete;
+
+    void push_front(T const &value)
+    {
+        std::unique_ptr<node> new_node(new node(value)); // 4
+        std::lock_guard<std::mutex> lk(head.m);
+        new_node->next = std::move(head.next); // 5
+        head.next = std::move(new_node);       // 6
+    }
+
+    template <typename Function>
+    void for_each(Function f) // 7
+    {
+        node *current = &head;
+        std::unique_lock<std::mutex> lk(head.m);       // 8
+        while (node *const next = current->next.get()) // 9
+        {
+            std::unique_lock<std::mutex> next_lk(next->m); // 10
+            lk.unlock();                                   // 11  f(*next->data); // 12
+            current = next;
+            lk = std::move(next_lk); // 13
+        }
+    }
+
+    template <typename Predicate>
+    std::shared_ptr<T> find_first_if(Predicate p) // 14
+    {
+        node *current = &head;
+        std::unique_lock<std::mutex> lk(head.m);
+        while (node *const next = current->next.get())
+        {
+            std::unique_lock<std::mutex> next_lk(next->m);
+            lk.unlock();
+            if (p(*next->data)) // 15
+            {
+                return next->data; // 16
+            }
+            current = next;
+            lk = std::move(next_lk);
+        }
+        return std::shared_ptr<T>();
+    }
+
+    template <typename Predicate>
+    void remove_if(Predicate p) // 17
+    {
+        node *current = &head;
+        std::unique_lock<std::mutex> lk(head.m);
+        while (node *const next = current->next.get())
+        {
+            std::unique_lock<std::mutex> next_lk(next->m);
+            if (p(*next->data)) // 18
+            {
+                std::unique_ptr<node> old_next = std::move(current->next);
+                current->next = std::move(next->next);
+                next_lk.unlock();
+            } // 20
+            else
+            {
+                lk.unlock(); // 21
+                current = next;
+                lk = std::move(next_lk);
+            }
+        }
+    }
+};
+#endif
+
+
+
 int main()
 {
+
     return 0;
 }
