86d06a57dbf0798bf56504653288b824fb37a86a
徐浩 <117811771+zyuhao7@users.noreply.github.com>
2024-07-16 20:31:42 +0800

    fast-start protobuf

---
+++ protobuf/fast_start/main.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+#include "contacts.pb.h"
+
+int main()
+{
+    std::string people_str;
+
+    {
+        // 对⼀个联系⼈的信息使⽤ PB 进⾏序列化，并将结果打印出来。
+        contacts::PeopleInfo people;
+        people.set_name("小明");
+        people.set_age(12);
+        if (!people.SerializeToString(&people_str))
+        {
+            std::cerr << "序列化联系⼈失败！" << std::endl;
+            return -1;
+        }
+        std::cout << "序列化成功，结果：" << people_str << std::endl;
+    }
+
+    {
+        // 对序列化后的内容使⽤ PB 进⾏反序列，解析出联系⼈信息并打印出来。
+        contacts::PeopleInfo people;
+        if (!people.ParseFromString(people_str))
+        {
+            std::cerr << "反序列化联系⼈失败！" << std::endl;
+            return -1;
+        }
+        std::cout << "反序列化成功！" << std::endl
+                  << "姓名： " << people.name() << std::endl
+                  << "年龄： " << people.age() << std::endl;
+    }
+
+    return 0;
+}
