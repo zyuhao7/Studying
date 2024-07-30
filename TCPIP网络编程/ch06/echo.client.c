7464734b4d91197b870377769335bb115675db25
徐浩 <117811771+zyuhao7@users.noreply.github.com>
2024-07-30 10:31:42 +0800

    TCPIP 网络编程 - 第六章 基于 UDP 的服务端/客户端

---
+++ TCPIP网络编程/ch06/echo.client.c
@@ -0,0 +1,57 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+
+#define BUF_SIZE 30
+void error_handling(char *msg);
+
+int main(int argc, char *argv[])
+{
+  int sock;
+  char msg[BUF_SIZE];
+  int str_len;
+
+  struct sockaddr_in serv_addr;
+  if (argc != 3)
+  {
+    printf("Usage: %s <IP> <port> \n", argv[0]);
+    exit(1);
+  }
+
+  // 创建 UDP 套接字.
+  sock = socket(PF_INET, SOCK_DGRAM, 0);
+  if (sock == -1)
+    error_handling("socket() error!");
+
+  memset(&serv_addr, 0, sizeof(serv_addr));
+  serv_addr.sin_family = AF_INET;
+  serv_addr.sin_addr.s_addr = inet_addr(argv[1]);
+  serv_addr.sin_port = htons(atoi(argv[2]));
+
+  connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
+  while (1)
+  {
+    fputs("Insert message(q to quit):", stdout);
+    fgets(msg, sizeof(msg), stdin);
+    if (!strcmp(msg, "q\n") || !strcmp(msg, "Q\n"))
+      break;
+
+    write(sock, msg, strlen(msg));
+
+    str_len = read(sock, msg, sizeof(msg) - 1);
+    msg[str_len] = 0;
+    printf("Message from server: %s\n", msg);
+  }
+  close(sock);
+  return 0;
+}
+
+void error_handling(char *msg)
+{
+  fputs(msg, stderr);
+  fputc('\n', stderr);
+  exit(1);
+}
