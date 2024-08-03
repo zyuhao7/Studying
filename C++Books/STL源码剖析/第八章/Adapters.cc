226f7887a88f2ce84a84a7fee56577131a47e25b
徐浩 <117811771+zyuhao7@users.noreply.github.com>
2024-08-03 10:45:52 +0800

    STL 源码剖析 第八章 - 配接器 Adapters

---
+++ C++Books/STL源码剖析/第八章/Adapters.cc
@@ -0,0 +1,117 @@
+#include <iostream>
+#include <functional>
+#include <queue>
+#include <vector>
+#include <iterator>
+#include <algorithm>
+using namespace std;
+//  配接器 adapter
+#if 1
+int main()
+{
+    ostream_iterator<int> outite(cout, " ");
+
+    int ia[] = {0, 1, 2, 3, 4, 5};
+    deque<int> id(ia, ia + 6);
+
+    copy(id.begin(), id.end(), outite); // 0 1 2 3 4 5
+    cout << endl;
+
+    copy(ia + 1, ia + 2, front_inserter(id));
+    copy(id.begin(), id.end(), outite); // 1 0 1 2 3 4 5
+    cout << endl;
+
+    copy(ia + 3, ia + 4, back_inserter(id));
+    copy(id.begin(), id.end(), outite); // 1 0 1 2 3 4 5 3
+    cout << endl;
+
+    deque<int>::iterator ite = find(id.begin(), id.end(), 5);
+    copy(ia, ia + 3, inserter(id, ite));
+    copy(id.begin(), id.end(), outite); // 1 0 1 2 3 4 0 1 2 5 3
+    cout << endl;
+
+    copy(id.rbegin(), id.rend(), outite); // 3 5 2 1 0 4 3 2 1 0 1
+    cout << endl;
+
+    return 0;
+}
+#endif
+
+#if 1
+int main()
+{
+    ostream_iterator<int> outite(cout, " ");
+
+    int ia[6] = {2, 21, 12, 7, 19, 23};
+    vector<int> iv(ia, ia + 6);
+
+    // 想对每个元素 v 执行 (v + 2) * 3
+    // for_each() 是 Immutable algorithm, 元素内容不能更改.
+    // 所以执行之后 iv 内容不变.
+
+    // for_each(iv.begin(), iv.end(), compose1(bind2nd(multiplies<int>(), 3), bind2nd(plus<int>(), 2)));
+
+    copy(iv.begin(), iv.end(), outite); // 2 21 12 7 19 23
+    cout << endl;
+
+    // transform(iv.begin(), iv.end(), outite, compose1(bind2nd(multiplies<int>(), 3), bind2nd(plus<int>(), 2)));
+
+    return 0;
+}
+#endif
+
+void print(int i)
+{
+    cout << i << " ";
+}
+
+class Int
+{
+public:
+    explicit Int(int i)
+        : m_i(i)
+    {
+    }
+
+    void print1() const
+    {
+        cout << "[" << m_i << "]";
+    }
+
+private:
+    int m_i;
+};
+
+#if 1
+int main()
+{
+    ostream_iterator<int> outite(cout, " ");
+    int ia[6] = {2, 21, 12, 7, 19, 23};
+    vector<int> iv(ia, ia + 6);
+
+    // 找出不小于 12 的元素个数.
+    cout << count_if(iv.begin(), iv.end(), not1(bind2nd(less<int>(), 12))); // 12
+    cout << endl;
+
+    // 将下列所有元素拷贝到 outite, 有很多方法.
+    copy(iv.begin(), iv.end(), outite); // 2 21 12 7 19 23
+    cout << endl;
+
+    for_each(iv.begin(), iv.end(), print); // 2 21 12 7 19 23
+    cout << endl;
+
+    for_each(iv.begin(), iv.end(), ptr_fun(print)); // 2 21 12 7 19 23
+    cout << endl;
+
+    Int t1(3), t2(7), t3(20), t4(14), t5(68);
+
+    vector<Int> Iv;
+    Iv.push_back(t1);
+    Iv.push_back(t2);
+    Iv.push_back(t3);
+    Iv.push_back(t4);
+    Iv.push_back(t5);
+
+    for_each(Iv.begin(), Iv.end(), mem_fun_ref(&Int::print1)); // [3][7][20][14][68]
+}
+#endif
