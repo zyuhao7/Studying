8194f3e6559b11eb4428dbb5871926c617c5ef20
徐浩 <117811771+zyuhao7@users.noreply.github.com>
2024-08-22 09:53:01 +0800

    protobuf 通讯录 2.4 map 实现备注信息

---
+++ protobuf/Contacts_2_4/write.cc
@@ -0,0 +1,152 @@
+#include <iostream>
+#include <fstream>
+#include "contacts.pb.h"
+using namespace std;
+using namespace contacts;
+
+// 新增联系人
+
+void AddPeopleInfo(PeopleInfo *people_info_ptr)
+{
+    cout << "------------新增联系人------------" << endl;
+
+    cout << "请输入联系人姓名:> ";
+    string name;
+    getline(cin, name);
+    people_info_ptr->set_name(name);
+
+    cout << "请输入联系人年龄:> ";
+    int age;
+    cin >> age;
+    people_info_ptr->set_age(age);
+    cin.ignore(256, '\n');
+
+    for (int i = 1;; i++)
+    {
+        cout << "请输入联系人电话 " << i << " (只输入回车完成电话新增): ";
+        string number;
+        getline(cin, number);
+        if (number.empty())
+            break;
+
+        PeopleInfo_Phone *phone = people_info_ptr->add_phone();
+        phone->set_number(number);
+
+        cout << "请输入该电话类型(1、移动电话   2、固定电话): ";
+        int type;
+        cin >> type;
+        cin.ignore(256, '\n');
+        switch (type)
+        {
+        case 1:
+            phone->set_type(PeopleInfo_Phone_PhoneType::PeopleInfo_Phone_PhoneType_MP);
+            break;
+        case 2:
+            phone->set_type(PeopleInfo_Phone_PhoneType::PeopleInfo_Phone_PhoneType_F_TEL);
+            break;
+        default:
+            cout << "选择有误！" << endl;
+            break;
+        }
+    }
+
+    Address addr;
+    cout << "请输入联系人家庭地址: ";
+
+    string home_addr;
+    getline(cin, home_addr);
+    addr.set_home_address(home_addr);
+
+    cout << "请输入联系人单位地址: ";
+    string unit_addr;
+    getline(cin, unit_addr);
+    addr.set_unit_address(unit_addr);
+
+    google::protobuf::Any *data = people_info_ptr->mutable_data();
+    data->PackFrom(addr);
+
+    cout << "请选择添加一个其他联系方式(1、 QQ号  2、 微信号): ";
+    int other_contact;
+    cin >> other_contact;
+    cin.ignore(256, '\n');
+    if (1 == other_contact)
+    {
+        cout << "请输入QQ号: ";
+        string qq;
+        getline(cin, qq);
+        people_info_ptr->set_qq(qq);
+    }
+    else if (2 == other_contact)
+    {
+        cout << "请输入微信号: ";
+        string Wx;
+        getline(cin, Wx);
+        people_info_ptr->set_weixin(Wx);
+    }
+    else
+    {
+        cout << "非法选择, 该设置失败! " << endl;
+    }
+
+    for (int i = 1;; i++)
+    {
+        cout << "请输入备注 " << i << "标题(只输入回车完成新增备注): ";
+        string remark_key;
+        getline(cin, remark_key);
+        if (remark_key.empty())
+            break;
+
+        cout << "请输入备注 " << i << "内容: ";
+        string remark_value;
+        getline(cin, remark_value);
+        people_info_ptr->mutable_remark()->insert({remark_key, remark_value});
+    }
+
+    cout << "------------添加联系人成功------------" << endl;
+}
+
+int main(int argc, char *argv[])
+{
+
+    GOOGLE_PROTOBUF_VERIFY_VERSION;
+
+    if (argc != 2)
+    {
+        cerr << "Usage: " << argv[0] << " CONTACTS_FILE" << endl;
+        return -1;
+    }
+
+    Contacts contacts;
+
+    // 先读取已存在的 contacts.
+    fstream input(argv[1], ios::in | ios::binary);
+    if (!input)
+    {
+        cout << argv[1] << ": File not found. Create a new file." << endl;
+    }
+    else if (!contacts.ParseFromIstream(&input))
+    {
+        cerr << "Failed to parse contacts." << endl;
+        input.close();
+        return -1;
+    }
+
+    // 新增一个联系人.
+    AddPeopleInfo(contacts.add_contacts());
+
+    // 向磁盘文件写入新的 contacts
+    fstream output(argv[1], ios::out | ios::trunc | ios::binary);
+    if (!contacts.SerializeToOstream(&output))
+    {
+        cerr << "Failed to write contacts. " << endl;
+        input.close();
+        output.close();
+        return -1;
+    }
+
+    input.close();
+    output.close();
+
+    google::protobuf::ShutdownProtobufLibrary();
+    return 0;
+}
