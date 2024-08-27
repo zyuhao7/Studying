14e1610844a6d1e0ac9d8803c8ef21df26385c43
徐浩 <117811771+zyuhao7@users.noreply.github.com>
2024-08-27 10:33:12 +0800

    剑指Offer 面试题 19 树的镜像 20 顺时针打印矩阵

--- Algorithm/剑指Offer/Exercise2.cc
+++ Algorithm/剑指Offer/Exercise2.cc
@@ -277,3 +277,130 @@ int main()
     return 0;
 }
 */
+
+// 2024-8-27
+// 面试题 19   二叉树的镜像
+/*
+struct BinaryTreeNode
+{
+    BinaryTreeNode(int val)
+        : _val(val),
+          _left(NULL),
+          _right(NULL)
+    {
+    }
+    int _val;
+    BinaryTreeNode *_left;
+    BinaryTreeNode *_right;
+};
+
+//         A                           B
+//         8                           8
+//      6     10                   10     6
+//    5   7  9   1               1   9   7  5
+// A 与 B 树 互为镜像.
+// 思路: 将根节点的左右指针交换, 然后递归左右子树.
+void PreorderTraversal(BinaryTreeNode *root)
+{
+    if (root == NULL)
+        return;
+    cout << root->_val << " ";
+    if (root->_left)
+        PreorderTraversal(root->_left);
+    if (root->_right)
+        PreorderTraversal(root->_right);
+}
+
+void MirrorRecursively(BinaryTreeNode *pNode)
+{
+    if (pNode == NULL || (pNode->_left == NULL && pNode->_right == NULL))
+        return;
+    BinaryTreeNode *tmp = pNode->_left;
+    pNode->_left = pNode->_right;
+    pNode->_right = tmp;
+    if (pNode->_left)
+        MirrorRecursively(pNode->_left);
+    if (pNode->_right)
+        MirrorRecursively(pNode->_right);
+}
+
+int main()
+{
+    BinaryTreeNode *b1 = new BinaryTreeNode(8);
+    BinaryTreeNode *b2 = new BinaryTreeNode(6);
+    BinaryTreeNode *b3 = new BinaryTreeNode(10);
+    BinaryTreeNode *b4 = new BinaryTreeNode(5);
+    BinaryTreeNode *b5 = new BinaryTreeNode(7);
+    BinaryTreeNode *b6 = new BinaryTreeNode(9);
+    BinaryTreeNode *b7 = new BinaryTreeNode(1);
+    b1->_left = b2, b1->_right = b3;
+    b2->_left = b4, b2->_right = b5;
+    b3->_left = b6, b3->_right = b7;
+    PreorderTraversal(b1);
+    cout << endl;
+    MirrorRecursively(b1);
+
+    PreorderTraversal(b1);
+
+    return 0;
+}
+*/
+
+// 面试题 20 顺时针打印矩阵
+// 1 2 3
+// 4 5 6
+// 7 8 9   -> 1 2 3 6 9 8 7 4 5 这种顺序.
+/*
+class Solution
+{
+public:
+    vector<int> spiralOrder(vector<vector<int>> &matrix)
+    {
+        vector<int> res;
+        if (matrix.empty())
+            return res;
+
+        int rows = matrix.size();
+        int cols = matrix[0].size();
+        int l = 0, r = cols - 1, t = 0, b = rows - 1;
+        while (l <= r && t <= b)
+        {
+            for (int i = l; i <= r; ++i)
+                res.push_back(matrix[t][i]);
+            for (int j = t + 1; j <= b; ++j)
+                res.push_back(matrix[j][r]);
+            if (l < r && t < b)
+            {
+                for (int i = r - 1; i >= l; --i)
+                    res.push_back(matrix[b][i]);
+                for (int j = b - 1; j > t; --j)
+                    res.push_back(matrix[j][l]);
+            }
+            l++;
+            r--;
+            t++;
+            b--;
+        }
+        return res;
+    }
+};
+int main()
+{
+    vector<vector<int>> v{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+
+    for (int i = 0; i < v.size(); ++i)
+    {
+        for (int j = 0; j < v[0].size(); ++j)
+            cout << v[i][j] << " ";
+        cout << endl;
+    }
+    Solution slt;
+    vector<int> res = slt.spiralOrder(v);
+    for (int i = 0; i < res.size(); ++i)
+        cout << res[i] << " ";
+    // 1 2 3
+    // 4 5 6
+    // 7 8 9
+    // 1 2 3 6 9 8 7 4 5
+}
+*/
