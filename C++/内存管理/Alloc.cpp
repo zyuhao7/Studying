f9f95cef340db2298989bfaa0fddeeb3f4bfdefe
徐浩 <117811771+zyuhao7@users.noreply.github.com>
2024-09-06 08:56:02 +0800

    C++ 内存管理

--- C++/内存管理/Alloc.cpp
+++ C++/内存管理/Alloc.cpp
@@ -7,7 +7,10 @@
 #include <cstring> //strlen()
 #include <type_traits>
 #include <memory>
+#include <vector>
 #include <ext/pool_allocator.h>
+#include <list>
+#include <cstdlib> // malloc free
 
 using namespace std;
 // day-2024-9-2
@@ -523,3 +526,281 @@ int main()
     return 0;
 }
 */
+
+// 2024-9-6
+
+// namespace myh
+// {
+
+//     class Bad
+//     {
+//     };
+//     class Foo
+//     {
+//     public:
+//         Foo() { cout << "Foo::Foo()" << endl; }
+//         Foo(int)
+//         {
+//             cout << "Foo::Foo(int)" << endl;
+//             // throw Bad();
+//         }
+
+//         //(1) 這個就是一般的 operator new() 的重載
+//         void *operator new(size_t size)
+//         {
+//             cout << "operator new(size_t size), size= " << size << endl;
+//             return malloc(size);
+//         }
+
+//         //(2) 這個就是標準庫已經提供的 placement new() 的重載 (形式)
+//         //    (所以我也模擬 standard placement new 的動作, just return ptr)
+//         void *operator new(size_t size, void *start)
+//         {
+//             cout << "operator new(size_t size, void* start), size= " << size << "  start= " << start << endl;
+//             return start;
+//         }
+
+//         //(3) 這個才是嶄新的 placement new
+//         void *operator new(size_t size, long extra)
+//         {
+//             cout << "operator new(size_t size, long extra)  " << size << ' ' << extra << endl;
+//             return malloc(size + extra);
+//         }
+
+//         //(4) 這又是一個 placement new
+//         void *operator new(size_t size, long extra, char init)
+//         {
+//             cout << "operator new(size_t size, long extra, char init)  " << size << ' ' << extra << ' ' << init << endl;
+//             return malloc(size + extra);
+//         }
+
+//         //(5) 這又是一個 placement new, 但故意寫錯第一參數的 type (它必須是 size_t 以滿足正常的 operator new)
+//         //!  	void* operator new(long extra, char init) {
+//         //!     [Error] 'operator new' takes type 'size_t' ('unsigned int') as first parameter [-fpermissive]
+//         //!	  	cout << "op-new(long,char)" << endl;
+//         //!    	return malloc(extra);
+//         //!  	}
+
+//         // 以下是搭配上述 placement new 的各個 called placement delete.
+//         // 當 ctor 發出異常，這兒對應的 operator (placement) delete 就會被喚起.
+//         // 應該是要負責釋放其搭檔兄弟 (placement new) 分配所得的 memory.
+//         //(1) 這個就是一般的 operator delete() 的重載
+//         void operator delete(void *, size_t)
+//         {
+//             cout << "operator delete(void*,size_t)  " << endl;
+//         }
+
+//         //(2) 這是對應上述的 (2)
+//         void operator delete(void *, void *)
+//         {
+//             cout << "operator delete(void*,void*)  " << endl;
+//         }
+
+//         //(3) 這是對應上述的 (3)
+//         void operator delete(void *, long)
+//         {
+//             cout << "operator delete(void*,long)  " << endl;
+//         }
+
+//         //(4) 這是對應上述的 (4)
+//         // 如果沒有一一對應, 也不會有任何編譯報錯
+//         void operator delete(void *, long, char)
+//         {
+//             cout << "operator delete(void*,long,char)  " << endl;
+//         }
+
+//     private:
+//         int m_i;
+//     };
+
+//     //-------------
+//     void test_overload_placement_new()
+//     {
+//         cout << "\n\n\ntest_overload_placement_new().......... \n";
+
+//         Foo start; // Foo::Foo
+
+//         Foo *p1 = new Foo;            // operator new(size_t size), size= 4
+//         Foo *p2 = new (&start) Foo;   // operator new(size_t size, void* start), size= 4  start= 0x7fff8331abac
+//         Foo *p3 = new (100) Foo;      // operator new(size_t size, long extra)  4 100
+//         Foo *p4 = new (100, 'a') Foo; // operator new(size_t size, long extra, char init)  4 100 a
+
+//         Foo *p5 = new (100) Foo(1);      // operator new(size_t size, long extra)  4 100
+//         Foo *p6 = new (100, 'a') Foo(1); // operator new(size_t size, long extra, char init)  4 100 a
+//         Foo *p7 = new (&start) Foo(1);   // operator new(size_t size, void* start), size= 4  start= 0x7ffcc183511c
+//         Foo *p8 = new Foo(1);            // operator new(size_t size), size= 4
+//     }
+// }
+
+// int main()
+// {
+//     myh::test_overload_placement_new();
+//     return 0;
+// }
+
+// void *myAlloc(size_t size)
+// {
+//     return malloc(size);
+// }
+
+// void myFree(void *ptr)
+// {
+//     return free(ptr);
+// }
+
+// // 我要設計一個可以累計總分配量和總釋放量的 operator new() / operator delete().
+// // 除非 user 直接使用 malloc/free, 否則都避不開它們, 這樣就可以累積總量.
+// static long long countNew = 0;
+// static long long countDel = 0;
+// static long long countArrayNew = 0;
+// static long long countArrayDel = 0;
+// static long long timesNew = 0;
+
+// // 小心，這影響無遠弗屆
+// // 它們不可被聲明於一個 namespace 內
+// // 以下是成功的, 但我暫時不想要它 (免得對我這整個測試程序帶來影響).
+// inline void *operator new(size_t size)
+// {
+//     cout << "myh global new(), \t" << size << "\t";
+//     countNew += size;
+//     ++timesNew;
+
+//     void *p = myAlloc(size);
+//     cout << p << endl;
+//     return p;
+// }
+
+// inline void *operator new[](size_t size)
+// {
+//     cout << "myh global new[](), \t" << size << "\t";
+//     countArrayNew += size;
+
+//     void *p = myAlloc(size);
+//     cout << p << endl;
+//     return p;
+// }
+
+// // 天啊, 以下(1)(2)可以並存並由(2)抓住流程 (但它對我這兒的測試無用).
+// // 當只存在 (1) 時, 抓不住流程.
+// // 在 class members 中二者只能擇一 (任一均可)
+// //(1)
+// inline void operator delete(void *ptr, size_t size)
+// {
+//     cout << "myh global delete(ptr,size), \t" << ptr << "\t" << size << endl;
+//     countDel += size;
+//     myFree(ptr);
+// }
+// //(2)
+// inline void operator delete(void *ptr)
+// {
+//     cout << "myh global delete(ptr), \t" << ptr << endl;
+//     myFree(ptr);
+// }
+
+// //(1)
+// inline void operator delete[](void *ptr, size_t size)
+// {
+//     cout << "myh global delete[](ptr,size), \t" << ptr << "\t" << size << endl;
+//     countArrayDel += size;
+//     myFree(ptr);
+// }
+// //(2)
+// inline void operator delete[](void *ptr)
+// {
+//     cout << "myh global delete[](ptr), \t" << ptr << endl;
+//     myFree(ptr);
+// }
+
+// // C++/11 alias template
+// template <typename T>
+// using listPool = list<T, __gnu_cxx::__pool_alloc<T>>;
+
+// namespace myh
+// {
+//     void test_overload_global_new()
+//     {
+//         cout << "\n\n\ntest_overload_global_new().......... \n";
+
+//         //***** 測試時, main() 中的其他測試全都 remark, 獨留本測試 *****
+//         {
+//             cout << "::countNew= " << ::countNew << endl; // 0
+//             cout << "::countDel= " << ::countDel << endl; // 0
+//             cout << "::timesNew= " << ::timesNew << endl; // 0
+
+//             string *p = new string("My name is Ace"); // myh global new(),       8       0x25a7c20 	(註：這是 string size)
+//                                                       // myh global new(),       39      0x25a7c40
+//             delete p;                                 // myh global delete(ptr),         0x25a7c40
+//                                                       // myh global delete(ptr),         0x25a7c20
+
+//             cout << "::countNew= " << ::countNew << endl; // 47 ==> 8 + 39
+//             cout << "::timesNew= " << ::timesNew << endl; // 2
+//             cout << "::countDel= " << ::countDel << endl; // 0 <== 本測試顯然我永遠觀察不到我所要觀察的
+//                                                           //       因為進不去 operator delete(ptr,size) 版
+
+//             p = new string[3]; // myh global new[](),     32      0x25a7c40 (註：其中內含 arraySize field: 8 bytes,
+//                                // 所以 32-8 = 24 ==> 8*3, 也就是 3 個 string 每個佔 8 bytes)
+
+//             delete[] p; //  myh global delete[](ptr),       0x180cc40
+
+//             cout << "::countNew= " << ::countNew << endl;           // 47 ==> 39 + 8
+//             cout << "::timesNew= " << ::timesNew << endl;           // 2
+//             cout << "::countArrayNew= " << ::countArrayNew << endl; // 32
+
+//             // 測試: global operator new 也會帶容器帶來影響
+//             vector<int> vec(10); // myh global new(),       40      0x1e6fc40  (註：10 ints)
+//                                  // 註：vector object 本身不是 dynamic allocated.
+//             vec.push_back(1);
+//             // myh global new(),       80      0x1e6fc70
+//             // myh global delete(ptr),         0x1e6fc40
+//             vec.push_back(1);
+//             vec.push_back(1);
+
+//             cout << "::countNew= " << ::countNew << endl; //  167 = 47 + 80 + 40
+//             cout << "::timesNew= " << ::timesNew << endl; //   4
+
+//             list<int> lst;                                // 註：list object 本身不是 dynamic allocated.
+//             lst.push_back(1);                             // myh global new(),       24      0x921c20	(註：每個 node是 24 bytes)
+//             lst.push_back(1);                             // myh global new(),       24      0x921cd0
+//             lst.push_back(1);                             // myh global new(),       24      0x921cf0
+//             cout << "::countNew= " << ::countNew << endl; // 226 ==>  239 = 167 + 24  + 24 + 24
+//             cout << "::timesNew= " << ::timesNew << endl; // 7
+
+//             // myh global delete(ptr),         0x921c20
+//             // myh global delete(ptr),         0x921cd0
+//             // myh global delete(ptr),         0x921cf0
+//             // myh global delete(ptr),         0x921c70
+//         }
+
+//         {
+//             // reset countNew
+//             countNew = 0;
+//             timesNew = 0;
+
+//             // list<double, __gnu_cxx::__pool_alloc<double>> lst;
+//             // 上一行改用 C++/11 alias template 來寫 :
+//             listPool<double> lst;
+
+//             for (int i = 0; i < 1000000; ++i)
+//                 lst.push_back(i);
+//             cout << "::countNew= " << ::countNew << endl; // 25087984 (注意, node 都不帶 cookie)
+//             cout << "::timesNew= " << ::timesNew << endl; // 122
+//         }
+
+//         {
+//             // reset countNew
+//             countNew = 0;
+//             timesNew = 0;
+//             list<double> lst;
+//             for (int i = 0; i < 1000000; ++i)
+//                 lst.push_back(i);
+//             cout << "::countNew= " << ::countNew << endl; // 24000000 (注意, node 都帶 cookie)
+//             cout << "::timesNew= " << ::timesNew << endl; // 1000000
+//         }
+//     }
+// }
+
+// int main()
+// {
+//     myh::test_overload_global_new();
+//     return 0;
+// }
