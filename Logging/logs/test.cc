872e3d28047c1cd756a0a32423afbad66a61584d
徐浩 <117811771+zyuhao7@users.noreply.github.com>
2024-09-18 14:57:05 +0800

    普通日志测试(不带日志器)

--- Logging/logs/test.cc
+++ Logging/logs/test.cc
@@ -8,16 +8,16 @@ using namespace std;
 int main()
 {
     // Log message to be used in all tests
-    std::string logger_name = "myh";
-    log::LogMsg msg(logger_name, "main.cc", 27, "Formatted log message test!", log::LogLevel::value::INFO);
+    // std::string logger_name = "myh";
+    // log::LogMsg msg(logger_name, "main.cc", 27, "Formatted log message test!", log::LogLevel::value::INFO);
 
     // Test 1 : Default Formatter and StdoutSink -lpthread
-    {
-        log::Formatter fmt;
-        string str = fmt.format(msg); // Format the log message
-        log::LogSink::ptr stdout_sink = log::SinkFactory::create<log::StdoutSink>();
-        stdout_sink->log(str.c_str(), str.size()); // Write to stdout
-    }
+    // {
+    //     log::Formatter fmt;
+    //     string str = fmt.format(msg); // Format the log message
+    //     log::LogSink::ptr stdout_sink = log::SinkFactory::create<log::StdoutSink>();
+    //     stdout_sink->log(str.c_str(), str.size()); // Write to stdout
+    // }
 
     // Test 2: Custom Formatter with file sink
     // {
@@ -29,10 +29,10 @@ int main()
 
     // Test 3: Rolling File Sink
     // {
-    //     log::Formatter fmt("[%d{%H:%M:%S}][%p] %m%n");                                          // Another custom pattern
-    //     string str = fmt.format(msg);                                                           // Format the log message
-    //     log::LogSink::ptr roll_sink = log::SinkFactory::create<log::RollSink>("roll_log_", 50); // Small max file size for testing
-    //     for (int i = 0; i < 10; ++i)
+    //     log::Formatter fmt("[%d{%H:%M:%S}][%p] %m%n");                                                    // Another custom pattern
+    //     string str = fmt.format(msg);                                                                     // Format the log message
+    //     log::LogSink::ptr roll_sink = log::SinkFactory::create<log::RollSink>("./logfile/roll_log_", 50); // Small max file size for testing
+    //     for (int i = 0; i < 10086; ++i)
     //     {
     //         string test_str = "Log entry #" + std::to_string(i) + ": " + str;
     //         roll_sink->log(test_str.c_str(), test_str.size()); // Write several entries to trigger rolling
