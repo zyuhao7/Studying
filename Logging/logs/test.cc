2c518280081d4f11c613245731dde290fc35d217
徐浩 <117811771+zyuhao7@users.noreply.github.com>
2024-09-18 16:15:33 +0800

    普通日志测试(不含日志器类设计)

--- Logging/logs/test.cc
+++ Logging/logs/test.cc
@@ -8,16 +8,16 @@ using namespace std;
 int main()
 {
     // Log message to be used in all tests
-    // std::string logger_name = "myh";
-    // log::LogMsg msg(logger_name, "main.cc", 27, "Formatted log message test!", log::LogLevel::value::INFO);
+    std::string logger_name = "myh";
+    log::LogMsg msg(logger_name, "main.cc", 27, "Formatted log message test!", log::LogLevel::value::INFO);
 
-    // Test 1 : Default Formatter and StdoutSink -lpthread
-    // {
-    //     log::Formatter fmt;
-    //     string str = fmt.format(msg); // Format the log message
-    //     log::LogSink::ptr stdout_sink = log::SinkFactory::create<log::StdoutSink>();
-    //     stdout_sink->log(str.c_str(), str.size()); // Write to stdout
-    // }
+    // Test 1 : Default Formatter and StdoutSink - lpthread
+    {
+        log::Formatter fmt;
+        string str = fmt.format(msg); // Format the log message
+        log::LogSink::ptr stdout_sink = log::SinkFactory::create<log::StdoutSink>();
+        stdout_sink->log(str.c_str(), str.size()); // Write to stdout
+    }
 
     // Test 2: Custom Formatter with file sink
     // {
