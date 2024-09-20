403de0bf7849e7a1934e7cca81561089bc99fc65
徐浩 <117811771+zyuhao7@users.noreply.github.com>
2024-09-20 18:47:47 +0800

    C++ 内存管理

--- C++/内存管理/Alloc.cpp
+++ C++/内存管理/Alloc.cpp
@@ -1038,4 +1038,479 @@ int main()
 //     return 0;
 // }
 
+// 2024-9-20
 
+// namespace myh
+// {
+// // 本處完全模仿 SGI STL, G2.92 的 std::alloc
+// // 放在 namespace 中因此和 std 不衝突
+// // 此手法和 G4.92 ext\__pool_alloc.h 也完全相同.
+// #define __THROW_BAD_ALLOC            \
+//     cerr << "out of memory" << endl; \
+//     exit(1)
+//     //----------------------------------------------
+//     // 第1級配置器。
+//     //----------------------------------------------
+//     template <int inst>
+//     class __malloc_alloc_template
+//     {
+//     private:
+//         static void *oom_malloc(size_t);
+//         static void *oom_realloc(void *, size_t);
+//         static void (*__malloc_alloc_oom_handler)();
+
+//     public:
+//         static void *allocate(size_t n)
+//         {
+//             void *result = malloc(n); // 直接使用 malloc()
+//             if (0 == result)
+//                 result = oom_malloc(n);
+//             return result;
+//         }
+//         static void deallocate(void *p, size_t /* n */)
+//         {
+//             free(p); // 直接使用 free()
+//         }
+//         static void *reallocate(void *p, size_t /* old_sz */, size_t new_sz)
+//         {
+//             void *result = realloc(p, new_sz); // 直接使用 realloc()
+//             if (0 == result)
+//                 result = oom_realloc(p, new_sz);
+//             return result;
+//         }
+//         static void (*set_malloc_handler(void (*f)()))()
+//         { // 類似 C++ 的 set_new_handler().
+//             void (*old)() = __malloc_alloc_oom_handler;
+//             __malloc_alloc_oom_handler = f;
+//             return (old);
+//         }
+//     };
+//     //----------------------------------------------
+//     template <int inst>
+//     void (*__malloc_alloc_template<inst>::__malloc_alloc_oom_handler)() = 0;
+
+//     template <int inst>
+//     void *__malloc_alloc_template<inst>::oom_malloc(size_t n)
+//     {
+//         void (*my_malloc_handler)();
+//         void *result;
+
+//         for (;;)
+//         { // 不斷嘗試釋放、配置、再釋放、再配置…
+//             my_malloc_handler = __malloc_alloc_oom_handler;
+//             if (0 == my_malloc_handler)
+//             {
+//                 __THROW_BAD_ALLOC;
+//             }
+//             (*my_malloc_handler)(); // 呼叫處理常式，企圖釋放記憶體
+//             result = malloc(n);     // 再次嘗試配置記憶體
+//             if (result)
+//                 return (result);
+//         }
+//     }
+
+//     template <int inst>
+//     void *__malloc_alloc_template<inst>::oom_realloc(void *p, size_t n)
+//     {
+//         void (*my_malloc_handler)();
+//         void *result;
+
+//         for (;;)
+//         { // 不斷嘗試釋放、配置、再釋放、再配置…
+//             my_malloc_handler = __malloc_alloc_oom_handler;
+//             if (0 == my_malloc_handler)
+//             {
+//                 __THROW_BAD_ALLOC;
+//             }
+//             (*my_malloc_handler)(); // 呼叫處理常式，企圖釋放記憶體。
+//             result = realloc(p, n); // 再次嘗試配置記憶體。
+//             if (result)
+//                 return (result);
+//         }
+//     }
+//     //----------------------------------------------
+//     typedef __malloc_alloc_template<0> malloc_alloc;
+
+//     template <class T, class Alloc>
+//     class simple_alloc
+//     {
+//     public:
+//         static T *allocate(size_t n)
+//         {
+//             return 0 == n ? 0 : (T *)Alloc::allocate(n * sizeof(T));
+//         }
+//         static T *allocate(void)
+//         {
+//             return (T *)Alloc::allocate(sizeof(T));
+//         }
+//         static void deallocate(T *p, size_t n)
+//         {
+//             if (0 != n)
+//                 Alloc::deallocate(p, n * sizeof(T));
+//         }
+//         static void deallocate(T *p)
+//         {
+//             Alloc::deallocate(p, sizeof(T));
+//         }
+//     };
+
+//     //----------------------------------------------
+//     // 第二級配置器
+//     //----------------------------------------------
+//     enum
+//     {
+//         __ALIGN = 8
+//     }; // 小區塊的上調邊界
+//     enum
+//     {
+//         __MAX_BYTES = 128
+//     }; // 小區塊的上限
+//     enum
+//     {
+//         __NFREELISTS = __MAX_BYTES / __ALIGN
+//     }; // free-lists 個數
+
+//     // 本例中兩個 template 參數完全沒有派上用場
+//     template <bool threads, int inst>
+//     class __default_alloc_template
+//     {
+//     private:
+//         // 實際上應使用 static const int x = N
+//         // 取代 enum { x = N }, 但目前支援該性質的編譯器不多
+
+//         static size_t ROUND_UP(size_t bytes)
+//         {
+//             return (((bytes) + __ALIGN - 1) & ~(__ALIGN - 1));
+//         }
+
+//     private:
+//         union obj
+//         {
+//             union obj *free_list_link;
+//         };
+
+//     private:
+//         static obj *volatile free_list[__NFREELISTS];
+//         static size_t FREELIST_INDEX(size_t bytes)
+//         {
+//             return (((bytes) + __ALIGN - 1) / __ALIGN - 1);
+//         }
+
+//         // Returns an object of size n, and optionally adds to size n free list.
+//         static void *refill(size_t n);
+
+//         // Allocates a chunk for nobjs of size "size".  nobjs may be reduced
+//         // if it is inconvenient to allocate the requested number.
+//         static char *chunk_alloc(size_t size, int &nobjs);
+
+//         // Chunk allocation state.
+//         static char *start_free;
+//         static char *end_free;
+//         static size_t heap_size;
+
+//     public:
+//         static void *allocate(size_t n) // n must be > 0
+//         {
+//             obj *volatile *my_free_list; // obj** my_free_list;
+//             obj *result;
+
+//             if (n > (size_t)__MAX_BYTES)
+//             {
+//                 return (malloc_alloc::allocate(n));
+//             }
+
+//             my_free_list = free_list + FREELIST_INDEX(n);
+//             result = *my_free_list;
+//             if (result == 0)
+//             {
+//                 void *r = refill(ROUND_UP(n));
+//                 return r;
+//             }
+
+//             *my_free_list = result->free_list_link;
+//             return (result);
+//         }
+
+//         static void deallocate(void *p, size_t n) // p may not be 0
+//         {
+//             obj *q = (obj *)p;
+//             obj *volatile *my_free_list; // obj** my_free_list;
+
+//             if (n > (size_t)__MAX_BYTES)
+//             {
+//                 malloc_alloc::deallocate(p, n);
+//                 return;
+//             }
+//             my_free_list = free_list + FREELIST_INDEX(n);
+//             q->free_list_link = *my_free_list;
+//             *my_free_list = q;
+//         }
+
+//         static void *reallocate(void *p, size_t old_sz, size_t new_sz);
+//     };
+
+//     //----------------------------------------------
+//     // We allocate memory in large chunks in order to
+//     // avoid fragmentingthe malloc heap too much.
+//     // We assume that size is properly aligned.
+//     // We hold the allocation lock.
+//     //----------------------------------------------
+//     template <bool threads, int inst>
+//     char *
+//     __default_alloc_template<threads, inst>::
+//         chunk_alloc(size_t size, int &nobjs)
+//     {
+//         char *result;
+//         size_t total_bytes = size * nobjs;
+//         size_t bytes_left = end_free - start_free;
+
+//         if (bytes_left >= total_bytes)
+//         {
+//             result = start_free;
+//             start_free += total_bytes;
+//             return (result);
+//         }
+//         else if (bytes_left >= size)
+//         {
+//             nobjs = bytes_left / size;
+//             total_bytes = size * nobjs;
+//             result = start_free;
+//             start_free += total_bytes;
+//             return (result);
+//         }
+//         else
+//         {
+//             size_t bytes_to_get =
+//                 2 * total_bytes + ROUND_UP(heap_size >> 4);
+//             // Try to make use of the left-over piece.
+//             if (bytes_left > 0)
+//             {
+//                 obj *volatile *my_free_list =
+//                     free_list + FREELIST_INDEX(bytes_left);
+
+//                 ((obj *)start_free)->free_list_link = *my_free_list;
+//                 *my_free_list = (obj *)start_free;
+//             }
+//             start_free = (char *)malloc(bytes_to_get);
+//             if (0 == start_free)
+//             {
+//                 int i;
+//                 obj *volatile *my_free_list, *p;
+
+//                 // Try to make do with what we have. That can't
+//                 // hurt. We do not try smaller requests, since that tends
+//                 // to result in disaster on multi-process machines.
+//                 for (i = size; i <= __MAX_BYTES; i += __ALIGN)
+//                 {
+//                     my_free_list = free_list + FREELIST_INDEX(i);
+//                     p = *my_free_list;
+//                     if (0 != p)
+//                     {
+//                         *my_free_list = p->free_list_link;
+//                         start_free = (char *)p;
+//                         end_free = start_free + i;
+//                         return (chunk_alloc(size, nobjs));
+//                         // Any leftover piece will eventually make it to the
+//                         // right free list.
+//                     }
+//                 }
+//                 end_free = 0; // In case of exception.
+//                 start_free = (char *)malloc_alloc::allocate(bytes_to_get);
+//                 // This should either throw an exception or
+//                 // remedy the situation. Thus we assume it
+//                 // succeeded.
+//             }
+//             heap_size += bytes_to_get;
+//             end_free = start_free + bytes_to_get;
+//             return (chunk_alloc(size, nobjs));
+//         }
+//     }
+
+//     //----------------------------------------------
+//     // Returns an object of size n, and optionally adds
+//     // to size n free list.We assume that n is properly aligned.
+//     // We hold the allocation lock.
+//     //----------------------------------------------
+//     template <bool threads, int inst>
+//     void *__default_alloc_template<threads, inst>::
+//         refill(size_t n)
+//     {
+//         int nobjs = 20;
+//         char *chunk = chunk_alloc(n, nobjs);
+//         obj *volatile *my_free_list; // obj** my_free_list;
+//         obj *result;
+//         obj *current_obj;
+//         obj *next_obj;
+//         int i;
+
+//         if (1 == nobjs)
+//             return (chunk);
+//         my_free_list = free_list + FREELIST_INDEX(n);
+
+//         // Build free list in chunk
+//         result = (obj *)chunk;
+//         *my_free_list = next_obj = (obj *)(chunk + n);
+//         for (i = 1;; ++i)
+//         {
+//             current_obj = next_obj;
+//             next_obj = (obj *)((char *)next_obj + n);
+//             if (nobjs - 1 == i)
+//             {
+//                 current_obj->free_list_link = 0;
+//                 break;
+//             }
+//             else
+//             {
+//                 current_obj->free_list_link = next_obj;
+//             }
+//         }
+//         return (result);
+//     }
+//     //----------------------------------------------
+//     template <bool threads, int inst>
+//     char *__default_alloc_template<threads, inst>::start_free = 0;
+
+//     template <bool threads, int inst>
+//     char *__default_alloc_template<threads, inst>::end_free = 0;
+
+//     template <bool threads, int inst>
+//     size_t __default_alloc_template<threads, inst>::heap_size = 0;
+
+//     template <bool threads, int inst>
+//     typename __default_alloc_template<threads, inst>::obj *volatile __default_alloc_template<threads, inst>::free_list[__NFREELISTS] = {
+//         0,
+//         0,
+//         0,
+//         0,
+//         0,
+//         0,
+//         0,
+//         0,
+//         0,
+//         0,
+//         0,
+//         0,
+//         0,
+//         0,
+//         0,
+//         0,
+//     };
+//     //----------------------------------------------
+//     // 令第2級配置器的名稱為 alloc
+//     typedef __default_alloc_template<false, 0> alloc;
+
+//     //**********************************
+//     void test_G29_alloc()
+//     {
+//         cout << "\n\n\ntest_global_allocator_with_16_freelist().......... \n";
+
+//         void *p1 = alloc::allocate(120);
+//         void *p2 = alloc::allocate(72);
+//         void *p3 = alloc::allocate(60); // 不是 8 倍數
+
+//         cout << p1 << ' ' << p2 << ' ' << p3 << endl; // 0x12260e0 0x1226a40 0x1226fe0
+
+//         alloc::deallocate(p1, 120);
+//         alloc::deallocate(p2, 72);
+//         alloc::deallocate(p3, 60);
+
+//         // 以下, 不能搭配容器來測試, 因為新版 G++ 對於 allocator 有更多要求 (詢問更多 typedef 而 alloc 都無法回答)
+//         // 它其實就是 G4.9 __pool_alloc，所以讓 G4.9容器使用 __pool_alloc 也就等同於這裡所要的測試
+
+//         // vector<int, simple_alloc<int, alloc>> v;
+//         // for (int i = 0; i < 1000; ++i)
+//         //     v.push_back(i);
+//         // for (int i = 700; i < 720; ++i)
+//         //     cout << v[i] << ' ';
+
+//         // cout << endl;
+//     }
+// }
+
+// int main()
+// {
+//     myh::test_G29_alloc();
+//     return 0;
+// }
+
+// namespace myh
+// {
+
+//     class Foo
+//     {
+//     public:
+//         long _x;
+
+//     public:
+//         Foo(long x = 0) : _x(x) {}
+
+//         //!	static void* operator new(size_t size) = default;	 				//[Error] cannot be defaulted
+//         //! static void  operator delete(void* pdead, size_t size) = default;	//[Error] cannot be defaulted
+//         static void *operator new[](size_t size) = delete;
+//         static void operator delete[](void *pdead, size_t size) = delete;
+//     };
+
+//     class Goo
+//     {
+//     public:
+//         long _x;
+
+//     public:
+//         Goo(long x = 0) : _x(x) {}
+
+//         static void *operator new(size_t size) = delete;
+//         static void operator delete(void *pdead, size_t size) = delete;
+//     };
+
+//     void test_delete_and_default_for_new()
+//     {
+//         cout << "\n\n\ntest_delete_and_default_for_new().......... \n";
+
+//         Foo *p1 = new Foo(5);
+//         delete p1;
+//         //! Foo *pF = new Foo[10]; //[Error] use of deleted function 'static void* jj12::Foo::operator new [](size_t)'
+//         //! delete[] pF;           //[Error] use of deleted function 'static void jj12::Foo::operator delete [](void*, size_t)'
+
+//         //! Goo* p2 = new Goo(7);	//[Error] use of deleted function 'static void* jj12::Goo::operator new(size_t)'
+//         //!	delete p2;				//[Error] use of deleted function 'static void jj12::Goo::operator delete(void*, size_t)'
+//         Goo *pG = new Goo[10];
+//         delete[] pG;
+//     }
+// } // namespace
+
+// int main()
+// {
+//     myh::test_delete_and_default_for_new();
+//     return 0;
+// }
+
+// #include <new>
+// #include <iostream>
+// #include <cassert>
+// namespace myh
+// {
+//     void noMoreMemory()
+//     {
+//         cerr << "out of memory";
+//         abort();
+//     }
+
+//     void test_set_new_handler()
+//     {
+//         cout << "\n\n\ntest_set_new_handler().......... \n";
+
+//         set_new_handler(noMoreMemory);
+
+//         // int *p = new int[100000000000000]; // well, so BIG!
+//         // assert(p);
+
+//         int *p = new int[100000000000000000000]; //[Warning] integer constant is too large for its type
+//         assert(p);
+//     }
+// } // namespace
+
+// int main()
+// {
+//     myh::test_set_new_handler();
+//     return 0;
+// }
