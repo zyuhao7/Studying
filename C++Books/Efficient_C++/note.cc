992a2ccab94f2ea3bc6b7f062c1df8ec46626d0a
徐浩 <117811771+zyuhao7@users.noreply.github.com>
2024-10-13 21:42:13 +0800

    第五章 临时对象 第六章 单线程内存池

--- C++Books/Efficient_C++/note.cc
+++ C++Books/Efficient_C++/note.cc
@@ -201,3 +201,287 @@ private:
 // 如果必须按值返回对象, 通过 RVO 可以省去创建和销毁局部对象的步骤, 从而改善性能.
 // RVO 的应用要遵循编译器的实现而定, 这需要考虑编译器文档或实验来判断是否使用 RVO 以及何时使用.
 // 通过编写计算性构造函数可以更好滴使用 RVO.
+
+// day-2024-10-13
+// 第五章 临时对象
+
+// 临时对象会以构造函数和析构函数的形式降低一半的性能.
+//  将构造函数声明为 explicit, 可以阻止编译器在幕后使用类型转换.
+// 编译器尝尝创建临时对象来解决类型不匹配问题, 通过函数重载可以避免这种情况.
+// 如果可能, 尽量避免使用对象拷贝, 按引用传递和返回对象.
+// 在 <op> 可能是 "+ - * / " 的地方, 使用 <op> = 运算符消除临时对象.
+
+// 第六章 单线程内存池
+// 版本 0 全局函数 new() 和 delete()
+
+// class Rational
+// {
+//     friend Rational operator+(const Rational &, const Rational &);
+
+// public:
+//     Rational(int a = 0, int b = 1)
+//         : m(a),
+//           n(b)
+//     {
+//     }
+
+// private:
+//     int m; // 分子
+//     int n; // 分母
+// };
+// 版本 1 专用 Rational 内存管理器
+
+// 将空闲列表声明为 Rational 类的静态成员, Rational的操作符 new() 和 delete() 可以管理该
+// 静态列表.
+
+// class NextOnFreeList
+// {
+// public:
+//     NextOnFreeList *next;
+// };
+
+// class Rational
+// {
+//     friend Rational operator+(const Rational &, const Rational &);
+
+// public:
+//     Rational(int a = 0, int b = 1)
+//         : m(a),
+//           n(b) {}
+//     inline void *operator new(size_t size);
+//     inline void operator delete(void *doomed, size_t size);
+
+//     static void newMemPool() { expandTheFreeList(); }
+//     static void deleteMemPool();
+
+// private:
+//     static void expandTheFreeList();
+//     enum
+//     {
+//         EXPANSION_SIZE = 32
+//     };
+//     int m;                           // 分子
+//     int n;                           // 分母
+//     static NextOnFreeList *freeList; // Rational 对象的空闲列表.
+// };
+
+// inline void *Rational::operator new(size_t size)
+// {
+//     if (0 == freeList) // 如果列表为空, 则将其填满.
+//     {
+//         expandTheFreeList();
+//     }
+//     NextOnFreeList *head = freeList;
+//     freeList = head->next;
+//     return head;
+// }
+
+// inline void Rational::operator delete(void *doomed, size_t size)
+// {
+//     NextOnFreeList *head = static_cast<NextOnFreeList *>(doomed);
+//     head->next = freeList;
+//     freeList = head;
+// }
+
+// void Rational::expandTheFreeList()
+// {
+//     // 我们必须分配足够大的对象以包含下一个指针.
+//     size_t size = (sizeof(Rational) > sizeof(NextOnFreeList *)) ? sizeof(Rational) : sizeof(NextOnFreeList *);
+
+//     NextOnFreeList *runner = reinterpret_cast<NextOnFreeList *>(new char[size]);
+//     freeList = runner;
+//     for (int i = 0; i < EXPANSION_SIZE; ++i)
+//     {
+//         runner->next = reinterpret_cast<NextOnFreeList *>(new char[size]);
+//         runner = runner->next;
+//     }
+//     runner->next = nullptr;
+// }
+
+// void Rational::deleteMemPool()
+// {
+//     NextOnFreeList *nextPtr;
+//     for (nextPtr = freeList; nextPtr != nullptr; nextPtr = freeList)
+//     {
+//         freeList = freeList->next;
+//         delete[] nextPtr;
+//     }
+// }
+
+// NextOnFreeList *Rational::freeList = nullptr;
+
+// 版本 2 固定大小对象的内存池
+
+// template <class T>
+// class MemoryPool
+// {
+// public:
+//     MemoryPool(size_t size = EXPANSION_SIZE);
+//     ~MemoryPool();
+
+//     // 从空闲列表中分配 T 元素.
+//     inline void *alloc(size_t size);
+
+//     // 返回 T 元素到空闲列表中
+//     inline void free(void *someElement);
+
+// private:
+//     // 空闲列表的下一个元素.
+//     MemoryPool<T> *next;
+
+//     // 如果空闲列表为空, 按该大小扩展它
+//     enum
+//     {
+//         EXPANSION_SIZE = 32
+//     };
+//     // 添加空闲元素到空闲列表
+//     void expandTheFreeList(int howMany = EXPANSION_SIZE);
+// };
+
+// template <class T>
+// MemoryPool<T>::MemoryPool(size_t size)
+// {
+//     expandTheFreeList(size);
+// }
+
+// // 析构函数遍历空闲列表并且删除所有元素
+// template <class T>
+// MemoryPool<T>::~MemoryPool()
+// {
+//     MemoryPool<T> *nextPtr = next;
+//     for (nextPtr = next; nextPtr != NULL; nextPtr = next)
+//     {
+//         next = next->next;
+//         delete nextPtr;
+//     }
+// }
+
+// // 成员函数 alloc() 为 T 元素分配足够大的空间. 如果空闲列表用尽, 则调用 expandTheFreeList() 来扩充它.
+// template <class T>
+// inline void *MemoryPool<T>::alloc(size_t)
+// {
+//     if (!next)
+//     {
+//         expandTheFreeList();
+//     }
+//     MemoryPool<T> *head = next;
+//     next = head->next;
+//     return head;
+// }
+
+// template <class T>
+// inline void MemoryPool<T>::free(void *doomed)
+// {
+//     MemoryPool<T> *head = static_cast<MemoryPool<T> *>(doomed);
+//     head->next = next;
+//     next = head;
+// }
+
+// // expandTheFreeList() 用来向空闲列表添加新元素. 首先从堆上分配新元素, 然后把他们连接到列表中.
+// template <class T>
+// void MemoryPool<T>::expandTheFreeList(int howMany)
+// {
+//     size_t size = (sizeof(T) > sizeof(MemoryPool<T> *)) ? sizeof(T) : sizeof(MemoryPool<T> *);
+
+//     MemoryPool<T> *runner = reinterpret_cast<MemoryPool<T> *>(new char[size]);
+
+//     next = runner;
+//     for (int i = 0; i < howMany; ++i)
+//     {
+//         runner->next = reinterpret_cast<MemoryPool<T> *>(new char[size]);
+//         runner = runner->next;
+//     }
+//     runner->next = nullptr;
+// }
+// class Rational
+// {
+// public:
+//     Rational(int a = 0, int b = 1)
+//         : n(a),
+//           d(b)
+//     {
+//     }
+//     void *operator new(size_t size)
+//     {
+//         return memPool->alloc(size);
+//     }
+
+//     void operator delete(void *doomed, size_t size)
+//     {
+//         memPool->free(doomed);
+//     }
+
+//     static void newMemPool() { memPool = new MemoryPool<Rational>; }
+//     static void deleteMemPool() { delete memPool; }
+
+// private:
+//     int n; // 分子
+//     int d; // 分母
+//     static MemoryPool<Rational> *memPool;
+// };
+
+// MemoryPool<Rational> *Rational::memPool = nullptr;
+
+// 版本 3 单线程可变大小内存管理器 .. todo.
+
+int main()
+{
+    // 测量全局函数 new() 和 delete() 的基准性能.
+    // Rational *array[1000];
+    // auto start = chrono::high_resolution_clock::now();
+    // for (int j = 0; j < 5000; j++)
+    // {
+    //     for (int i = 0; i < 1000; ++i)
+    //     {
+    //         array[i] = new Rational(i);
+    //     }
+    //     for (int i = 0; i < 1000; ++i)
+    //     {
+    //         delete array[i];
+    //     }
+    // }
+    // auto end = chrono::high_resolution_clock::now();
+    // auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+    // std::cout << "spends: " << duration.count() << " milliseconds" << std::endl;
+    // 版本 0  212 mil
+
+    // Rational *array[1000];
+    // Rational::newMemPool();
+    // auto start = chrono::high_resolution_clock::now();
+    // for (int j = 0; j < 5000; j++)
+    // {
+    //     for (int i = 0; i < 1000; ++i)
+    //     {
+    //         array[i] = new Rational(i);
+    //     }
+    //     for (int i = 0; i < 1000; ++i)
+    //     {
+    //         delete array[i];
+    //     }
+    // }
+    // auto end = chrono::high_resolution_clock::now();
+    // auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+    // std::cout << "spends: " << duration.count() << " milliseconds" << std::endl; // 版本 1 28 mil
+    // Rational::deleteMemPool();
+
+    // Rational *array[1000];
+    // Rational::newMemPool();
+    // auto start = chrono::high_resolution_clock::now();
+    // for (int j = 0; j < 5000; j++)
+    // {
+    //     for (int i = 0; i < 1000; ++i)
+    //     {
+    //         array[i] = new Rational(i);
+    //     }
+    //     for (int i = 0; i < 1000; ++i)
+    //     {
+    //         delete array[i];
+    //     }
+    // }
+    // auto end = chrono::high_resolution_clock::now();
+    // auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+    // std::cout << "spends: " << duration.count() << " milliseconds" << std::endl; // 版本 2  41 mil
+    // Rational::deleteMemPool();
+
+    return 0;
+}
