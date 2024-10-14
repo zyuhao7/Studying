f6c84f05f89af5d64482680a1fa3b1a2e8eb2fb9
徐浩 <117811771+zyuhao7@users.noreply.github.com>
2024-10-14 22:46:06 +0800

    第 2 章 套接字类型与协议设置

---
+++ LinuxBooks/TCPIP网络编程/ch02/ch02/tcp_server.cc
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <iostream>
+void error_handling(char *message);
+using namespace std;
+int main(int argc, char *argv[])
+{
+    int serv_sock;
+    int client_sock;
+
+    struct sockaddr_in serv_addr;
+    struct sockaddr_in client_addr;
+
+    socklen_t client_addr_len;
+
+    char msg[] = "hello world!";
+    if (argc != 2)
+    {
+        printf("Usage : %s <port> \n", argv[0]);
+    }
+
+    // 调用 socket 函数创建套接字
+    serv_sock = socket(PF_INET, SOCK_STREAM, 0);
+    if (serv_sock == -1)
+        error_handling("socket() error");
+
+    memset(&serv_addr, 0, sizeof(serv_addr));
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    serv_addr.sin_port = htons(atoi(argv[1]));
+
+    if (bind(serv_sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) == -1)
+        error_handling("bind() error");
+
+    if (listen(serv_sock, 5) == -1)
+        error_handling("listen() error");
+
+    client_addr_len = sizeof(client_addr);
+    client_sock = accept(serv_sock, (struct sockaddr *)&client_addr, &client_addr_len);
+    if (client_sock == -1)
+        error_handling("accept() error!");
+
+    write(client_sock, msg, sizeof(msg));
+    close(client_sock);
+    close(serv_sock);
+    return 0;
+}
+
+void error_handling(char *message)
+{
+    fputs(message, stderr);
+    fputc('\n', stderr);
+    exit(1);
+}
