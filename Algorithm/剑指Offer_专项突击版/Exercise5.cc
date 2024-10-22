1d2e5453b073f60b4be79893b8fd8cb7c1802d98
徐浩 <117811771+zyuhao7@users.noreply.github.com>
2024-10-22 23:19:14 +0800

    062. 实现前缀树

--- Algorithm/剑指Offer_专项突击版/Exercise5.cc
+++ Algorithm/剑指Offer_专项突击版/Exercise5.cc
@@ -192,7 +192,72 @@ using namespace std;
 //     }
 // };
 
+// day-2024-10-22
 // 剑指 Offer II 062. 实现前缀树
+// struct TrieNode
+// {
+//     TrieNode *child[26];
+//     bool isEnd;
+
+//     TrieNode()
+//         : isEnd(false)
+//     {
+//         for (int i = 0; i < 26; ++i)
+//         {
+//             child[i] = nullptr;
+//         }
+//     }
+// };
+
+// class Trie
+// {
+// public:
+//     TrieNode *root;
+
+//     Trie()
+//     {
+//         root = new TrieNode();
+//     }
+
+//     void insert(string word)
+//     {
+//         TrieNode *node = root;
+//         for (char c : word)
+//         {
+//             int i = c - 'a';
+//             if (node->child[i] == nullptr)
+//                 node->child[i] = new TrieNode();
+//             node = node->child[i];
+//         }
+//         node->isEnd = true;
+//     }
+
+//     bool search(string word)
+//     {
+//         TrieNode *node = root;
+//         for (auto &c : word)
+//         {
+//             int i = c - 'a';
+//             if (node->child[i] == nullptr)
+//                 return false;
+//             node = node->child[i];
+//         }
+//         return node->isEnd;
+//     }
+
+//     bool startsWith(string prefix)
+//     {
+//         TrieNode *node = root;
+//         for (auto &c : prefix)
+//         {
+//             int i = c - 'a';
+//             if (node->child[i] == nullptr)
+//                 return false;
+//             node = node->child[i];
+//         }
+//         return true;
+//     }
+// };
 
 int main()
 {
