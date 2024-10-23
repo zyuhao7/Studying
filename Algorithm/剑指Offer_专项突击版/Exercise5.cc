a83743ee06e9ffafcd2af1eab3aad833d058eede
徐浩 <117811771+zyuhao7@users.noreply.github.com>
2024-10-23 23:15:03 +0800

    063	替换单词  064	神奇的字典 065  最短的单词编码

--- Algorithm/剑指Offer_专项突击版/Exercise5.cc
+++ Algorithm/剑指Offer_专项突击版/Exercise5.cc
@@ -259,6 +259,273 @@ using namespace std;
 //     }
 // };
 
+// day-2024-10-23
+// 剑指 Offer II 063. 替换单词              648. 单词替换
+// struct TrieNode
+// {
+//     TrieNode *child[26];
+//     bool isEnd;
+
+//     TrieNode()
+//     {
+//         isEnd = false;
+//         for (int i = 0; i < 26; i++)
+//         {
+//             child[i] = nullptr;
+//         }
+//     }
+// };
+
+// class Trie
+// {
+//     TrieNode *root;
+
+// public:
+//     Trie()
+//     {
+//         root = new TrieNode();
+//     }
+
+//     void insert(const string &word)
+//     {
+//         TrieNode *node = root;
+//         for (char c : word)
+//         {
+//             int i = c - 'a';
+//             if (node->child[i] == nullptr)
+//             {
+//                 node->child[i] = new TrieNode();
+//             }
+//             node = node->child[i];
+//         }
+//         node->isEnd = true;
+//     }
+
+//     string startsWith(const string &prefix)
+//     {
+//         TrieNode *node = root;
+//         string result;
+//         for (char c : prefix)
+//         {
+//             int i = c - 'a';
+//             if (node->child[i] == nullptr)
+//             {
+//                 return "";
+//             }
+//             result += c;
+//             node = node->child[i];
+//             if (node->isEnd)
+//             {
+//                 return result;
+//             }
+//         }
+//         return "";
+//     }
+// };
+
+// class Solution
+// {
+// public:
+//     string replaceWords(vector<string> &dictionary, string sentence)
+//     {
+//         Trie trie;
+//         for (const auto &str : dictionary)
+//         {
+//             trie.insert(str);
+//         }
+
+//         string res;
+//         string tmp;
+//         int j = 0;
+
+//         for (int i = 0; i <= sentence.size(); ++i)
+//         {
+//             if (i == sentence.size() || sentence[i] == ' ')
+//             {
+//                 tmp = sentence.substr(j, i - j);
+//                 j = i + 1;
+//                 string prefix = trie.startsWith(tmp);
+//                 if (prefix.empty())
+//                 {
+//                     res += tmp; // 如果不存在词根, 则加入原串.
+//                 }
+//                 else
+//                 {
+//                     res += prefix; // 否则加入词根
+//                 }
+//                 if (i != sentence.size())
+//                 {
+//                     res += ' ';
+//                 }
+//             }
+//         }
+//         return res;
+//     }
+// };
+
+// 剑指 Offer II 064. 神奇的字典
+// struct TrieNode
+// {
+//     TrieNode *child[26];
+//     bool isEnd;
+//     TrieNode()
+//     {
+//         isEnd = false;
+//         for (int i = 0; i < 26; i++)
+//         {
+//             child[i] = nullptr;
+//         }
+//     }
+// };
+
+// class Trie
+// {
+// private:
+//     TrieNode *root;
+
+// public:
+//     Trie()
+//     {
+//         root = new TrieNode();
+//     }
+
+//     // 插入一个单词
+//     void insert(const string &word)
+//     {
+//         TrieNode *node = root;
+//         for (char c : word)
+//         {
+//             int i = c - 'a';
+//             if (node->child[i] == nullptr)
+//             {
+//                 node->child[i] = new TrieNode();
+//             }
+//             node = node->child[i];
+//         }
+//         node->isEnd = true;
+//     }
+
+//     bool searchWithOneDifference(TrieNode *node, const string &word, int pos, bool modified)
+//     {
+//         if (pos == word.size())
+//             return modified && node->isEnd;
+
+//         int i = word[pos] - 'a';
+//         if (node->child[i])
+//         {
+//             if (searchWithOneDifference(node->child[i], word, pos + 1, modified))
+//                 return true;
+//         }
+//         if (!modified)
+//         {
+//             for (int j = 0; j < 26; ++j)
+//             {
+//                 if (j != i && node->child[j])
+//                     if (searchWithOneDifference(node->child[j], word, pos + 1, true))
+//                         return true;
+//             }
+//         }
+
+//         return false;
+//     }
+
+//     bool query(const string &word)
+//     {
+//         return searchWithOneDifference(root, word, 0, false);
+//     }
+// };
+
+// class MagicDictionary
+// {
+// public:
+//     Trie trie;
+//     MagicDictionary()
+//     {
+//     }
+
+//     void buildDict(vector<string> dictionary)
+//     {
+//         for (auto str : dictionary)
+//         {
+//             trie.insert(str);
+//         }
+//     }
+
+//     bool search(string Word)
+//     {
+//         return trie.query(Word);
+//     }
+// };
+
+// 剑指 Offer II 065. 最短的单词编码
+// class TrieNode
+// {
+// public:
+//     bool isEnd;
+//     TrieNode *child[26];
+//     TrieNode()
+//     {
+//         isEnd = false;
+//         for (int i = 0; i < 26; ++i)
+//             child[i] = nullptr;
+//     }
+// };
+
+// class Trie
+// {
+// private:
+//     TrieNode *root;
+
+// public:
+//     Trie()
+//     {
+//         root = new TrieNode();
+//     }
+
+//     bool insert(const string &word)
+//     {
+//         TrieNode *node = root;
+//         bool isNewWord = false;
+
+//         for (char c : word)
+//         {
+//             int i = c - 'a';
+//             if (!node->child[i])
+//             {
+//                 node->child[i] = new TrieNode();
+//                 isNewWord = true;
+//             }
+//             node = node->child[i];
+//         }
+//         node->isEnd = true;
+//         return isNewWord;
+//     }
+// };
+// class Solution
+// {
+// public:
+//     int minimumLengthEncoding(vector<string> &words)
+//     {
+//         sort(words.begin(), words.end(), [](const string &a, const string &b)
+//              { return a.size() > b.size(); });
+
+//         Trie trie;
+//         int totalLen = 0;
+
+//         for (const string &str : words)
+//         {
+//             string reverseWord = str;
+//             reverse(reverseWord.begin(), reverseWord.end());
+
+//             if (trie.insert(reverseWord))
+//             {
+//                 totalLen += reverseWord.size() + 1;
+//             }
+//         }
+//         return totalLen;
+//     }
+// };
+
 int main()
 {
 
