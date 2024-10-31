7226dbf94ca10e77c677ec9dfd88dc4a17ff9e0e
徐浩 <117811771+zyuhao7@users.noreply.github.com>
2024-10-31 20:07:24 +0800

    085	生成匹配的括号    086	分割回文子字符串

--- Algorithm/剑指Offer_专项突击版/Exercise7.cc
+++ Algorithm/剑指Offer_专项突击版/Exercise7.cc
@@ -291,6 +291,72 @@ using namespace std;
 //     }
 // };
 
+// day-2024-10-31
+// 剑指 Offer II 085. 生成匹配的括号
+// class Solution
+// {
+// public:
+//     vector<string> generateParenthesis(int n)
+//     {
+//         vector<string> ans;
+//         dfs(0, 0, "", n, ans);
+//         return ans;
+//     }
+//     void dfs(int l, int r, string s, int n, vector<string> &ans)
+//     {
+//         if (n == l && n == r)
+//         {
+//             ans.push_back(s);
+//             return;
+//         }
+//         if (l < n)
+//             dfs(l + 1, r, s + '(', n, ans);
+//         if (r < l)
+//             dfs(l, r + 1, s + ')', n, ans);
+//     }
+// };
+
+// 剑指 Offer II 086. 分割回文子字符串
+// class Solution
+// {
+// public:
+//     vector<vector<string>> partition(string s)
+//     {
+//         vector<vector<string>> ans;
+//         vector<string> path;
+//         dfs(s, 0, ans, path);
+//         return ans;
+//     }
+//     void dfs(string &s, int idx, vector<vector<string>> &ans, vector<string> &path)
+//     {
+//         if (idx == s.size())
+//         {
+//             ans.push_back(path);
+//             return;
+//         }
+//         for (int i = idx; i < s.size(); ++i)
+//         {
+//             if (isPalindrome(s, idx, i))
+//             {
+//                 path.push_back(s.substr(idx, i - idx + 1));
+//                 dfs(s, i + 1, ans, path);
+//                 path.pop_back();
+//             }
+//         }
+//     }
+//     bool isPalindrome(string &s, int l, int r)
+//     {
+//         while (l < r)
+//         {
+//             if (s[l] != s[r])
+//                 return false;
+//             l++;
+//             r--;
+//         }
+//         return true;
+//     }
+// };
+
 int main()
 {
 
