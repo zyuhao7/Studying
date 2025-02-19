67cb3869d3aa2869cffed9743843023e87bfcc81
徐浩 <117811771+zyuhao7@users.noreply.github.com>
2025-02-19 20:51:39 +0800

    组合总和 Ⅳ    最小路径和

--- Algorithm/LeetCode题解/动规/dp.cc
+++ Algorithm/LeetCode题解/动规/dp.cc
@@ -158,7 +158,78 @@ using namespace std;
 //     }
 // };
 
+// day-2025-2-19
+// 377 组合总和 Ⅳ
+// class Solution
+// {
+// public:
+//     int combinationSum4(vector<int> &nums, int target)
+//     {
+//         vector<unsigned> dp(target + 1);
+//         dp[0] = 1;
+//         for (int i = 1; i <= target; ++i)
+//         {
+//             for (auto num : nums)
+//             {
+//                 if (i >= num)
+//                     dp[i] += dp[i - num];
+//             }
+//         }
+//         return dp[target];
+//     }
+// };
+
+// 64   最小路径和
+// class Solution {
+//     public:
+//         int minPathSum(vector<vector<int>>& grid) {
+//             int m = grid.size(), n = grid[0].size();
+//             vector<vector<int>> dp(m, vector<int>(n));
+//             for(int i = 0; i < m; ++i)
+//             {
+//                 for(int j = 0; j < n; ++j)
+//                 {
+//                     if(i == 0 && j == 0) dp[i][j] = grid[i][j];
+//                     else if(i == 0)
+//                         dp[i][j] = dp[i][j - 1] + grid[i][j];
+//                     else if(j == 0)
+//                         dp[i][j] = dp[i - 1][j] + grid[i][j];
+//                     else
+//                         dp[i][j] = min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
+//                 }
+//             }
+//             return dp[m - 1][n - 1];
+//         }
+//     };
+
+// 滚动数组
+// class Solution
+// {
+// public:
+//     int minPathSum(vector<vector<int>> &grid)
+//     {
+//         int m = grid.size(), n = grid[0].size();
+//         vector<int> dp(n);
+//         for (int i = 0; i < m; ++i)
+//         {
+//             for (int j = 0; j < n; ++j)
+//             {
+//                 if (i == 0 && j == 0)
+//                     dp[j] = grid[i][j];
+//                 else if (i == 0)
+//                     dp[j] = dp[j - 1] + grid[i][j];
+//                 else if (j == 0)
+//                     dp[j] += grid[i][j];
+//                 else
+//                     dp[j] = min(dp[j], dp[j - 1]) + grid[i][j];
+//             }
+//         }
+//         return dp[n - 1];
+//     }
+// };
+
 int main()
 {
+
     return 0;
 }
