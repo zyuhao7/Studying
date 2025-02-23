41ff31dbea5efd79759edfa5edf0ae47a8adc37e
徐浩 <117811771+zyuhao7@users.noreply.github.com>
2025-02-23 20:41:49 +0800

    类和对象 review

---
+++ C++/基础/类与对象/class.cc
@@ -0,0 +1,53 @@
+#include <iostream>
+using namespace std;
+// day-2025-2-23
+// class A1
+// {
+// public:
+//     void f1() {}
+
+// private:
+//     int _a;
+// };
+// class A2
+// {
+// public:
+//     void f2() {}
+// };
+
+// class A3
+// {
+// };
+// int main()
+// {
+//     cout << sizeof(A1) << " "; // 4
+//     cout << sizeof(A2) << " "; // 1
+//     cout << sizeof(A3) << " "; // 1
+
+//     return 0;
+// }
+
+// class A
+
+// {
+// public:
+//     void Print()
+//     {
+//         cout << "Print()" << endl;
+//     }
+//     void PrintA()
+//     {
+//         cout << _a << endl;
+//     }
+
+// private:
+//     int _a;
+// };
+
+// int main()
+// {
+//     A *p = nullptr;
+//     p->Print();  // 正常运行
+//     p->PrintA(); //  运行崩溃
+//     return 0;
+// }
