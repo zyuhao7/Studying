127b5a03d1bffec956b8ed39c72b2899a0ab54d1
徐浩 <117811771+zyuhao7@users.noreply.github.com>
2025-02-24 08:58:26 +0800

    class and object review

--- C++/基础/类与对象/class.cc
+++ C++/基础/类与对象/class.cc
@@ -51,3 +51,87 @@ using namespace std;
 //     p->PrintA(); //  运行崩溃
 //     return 0;
 // }
+
+// day-2025-2-24
+// class Date
+// {
+// public:
+//     Date()
+//     {
+//         _year = 1900;
+//         _month = 1;
+//         _day = 1;
+//     }
+//     Date(int year = 1900, int month = 1, int day = 1)
+//     {
+//         _year = year;
+//         _month = month;
+//         _day = day;
+//     }
+
+// private:
+//     int _year;
+//     int _month;
+//     int _day;
+// };
+
+// // 以下测试函数能通过编译吗？
+
+// void Test()
+// {
+//     Date d1;
+// }
+
+// int main()
+// {
+//     Test(); // 编译出错, call of overloaded 'Date()' is ambiguous
+//     return 0;
+// }
+
+// class Date
+// {
+// public:
+//     Date(int year, int month, int day)
+//     {
+//         _year = year;
+//         _month = month;
+//         _day = day;
+//     }
+//     void Print()
+//     {
+//         cout << "Print()" << endl;
+//         cout << "year:" << _year << endl;
+//         cout << "month:" << _month << endl;
+//         cout << "day:" << _day << endl
+//              << endl;
+//     }
+//     void Print() const
+
+//     {
+//         cout << "Print()const" << endl;
+//         cout << "year:" << _year << endl;
+//         cout << "month:" << _month << endl;
+//         cout << "day:" << _day << endl
+//              << endl;
+//     }
+
+// private:
+//     int _year;  // 年
+//     int _month; // 月
+//     int _day;   // 日
+// };
+
+// void Test()
+// {
+//     Date d1(2022, 1, 13);
+//     d1.Print(1); // 调用Print()
+//     const Date d2(2022, 1, 13);
+//     d2.Print(); // 调用Print()const
+// }
+
+// int main()
+// {
+//     Test();
+//     return 0;
+// }
+
