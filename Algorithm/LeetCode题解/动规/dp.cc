2a227c7f269b7a488eab61ce4f2444cd4c98eafd
徐浩 <117811771+zyuhao7@users.noreply.github.com>
2025-03-01 09:21:53 +0800

    最佳买卖股票时机含冷冻期   买卖股票的最佳时机含手续费

--- Algorithm/LeetCode题解/动规/dp.cc
+++ Algorithm/LeetCode题解/动规/dp.cc
@@ -753,3 +753,44 @@ using namespace std;
 //         return sell[k];
 //     }
 // };
+
+// day-2025-3-1
+//  309  买卖股票的最佳时机含冷冻期
+// class Solution
+// {
+// public:
+//     int maxProfit(vector<int> &prices)
+//     {
+//         int n = prices.size();
+//         vector<vector<int>> dp(n, vector<int>(2));
+//         dp[0][0] = 0, dp[0][1] = -prices[0];
+//         for (int i = 1; i < n; ++i)
+//         {
+//             dp[i][0] = max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
+//             if (i == 1)
+//                 dp[i][1] = max(dp[i - 1][1], -prices[i]);
+//             else
+//                 dp[i][1] = max(dp[i - 1][1], dp[i - 2][0] - prices[i]);
+//         }
+//         return dp[n - 1][0];
+//     }
+// };
+
+// 714. 买卖股票的最佳时机含手续费
+// class Solution
+// {
+// public:
+//     int maxProfit(vector<int> &prices, int fee)
+//     {
+//         int n = prices.size();
+//         vector<vector<int>> dp(n, vector<int>(2));
+//         dp[0][1] -= prices[0];
+//         for (int i = 1; i < n; i++)
+//         {
+//             dp[i][0] = max(dp[i - 1][0], dp[i - 1][1] + prices[i] - fee);
+//             dp[i][1] = max(dp[i - 1][1], dp[i - 1][0] - prices[i]);
+//         }
+//         return dp[n - 1][0];
+//     }
+// };
+
