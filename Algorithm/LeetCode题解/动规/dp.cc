68e4d802365d94ae6a8378d44ba114afac09824c
徐浩 <117811771+zyuhao7@users.noreply.github.com>
2025-03-02 14:07:53 +0800

    不相交的线、判断子序列、不同子序列、编辑距离

--- Algorithm/LeetCode题解/动规/dp.cc
+++ Algorithm/LeetCode题解/动规/dp.cc
@@ -822,3 +822,96 @@ using namespace std;
 //         return asc_count - 2 * dp[n1][n2];
 //     }
 // };
+
+// day-2025-3-2
+// 1035  不相交的线
+// class Solution
+// {
+// public:
+//     int maxUncrossedLines(vector<int> &nums1, vector<int> &nums2)
+//     {
+//         int n1 = nums1.size(), n2 = nums2.size();
+//         vector<vector<int>> dp(n1 + 1, vector<int>(n2 + 1));
+//         for (int i = 1; i <= n1; ++i)
+//         {
+//             for (int j = 1; j <= n2; ++j)
+//             {
+//                 if (nums1[i - 1] == nums2[j - 1])
+//                 {
+//                     dp[i][j] = dp[i - 1][j - 1] + 1;
+//                 }
+//                 else
+//                     dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+//             }
+//         }
+//         return dp[n1][n2];
+//     }
+// };
+
+// 392. 判断子序列
+// class Solution
+// {
+// public:
+//     bool isSubsequence(string s, string t)
+//     {
+//         if (s.empty())
+//             return true;
+//         int i = 0;
+//         for (auto c : t)
+//             if (c == s[i] && ++i == s.length())
+//                 return true;
+//         return false;
+//     }
+// };
+
+// 115. 不同的子序列
+// class Solution
+// {
+// public:
+//     int mod = 1e9 + 7;
+//     int numDistinct(string s, string t)
+//     {
+//         int m = s.size(), n = t.size();
+//         vector<vector<int>> dp(m + 1, vector<int>(n + 1));
+//         for (int i = 0; i <= m; ++i)
+//             dp[i][0] = 1;
+//         for (int i = 1; i <= m; ++i)
+//         {
+//             for (int j = 1; j <= i && j <= n; ++j)
+//             {
+//                 if (s[i - 1] == t[j - 1])
+//                 {
+//                     dp[i][j] = (dp[i - 1][j - 1] % mod + dp[i - 1][j] % mod) % mod;
+//                 }
+//                 else
+//                     dp[i][j] = dp[i - 1][j] % mod;
+//             }
+//         }
+//         return dp[m][n];
+//     }
+// };
+// 72. 编辑距离
+// class Solution
+// {
+// public:
+//     int minDistance(string word1, string word2)
+//     {
+//         int m = word1.size(), n = word2.size();
+//         vector<vector<int>> dp(m + 1, vector<int>(n + 1));
+//         for (int i = 0; i <= m; ++i)
+//             dp[i][0] = i;
+//         for (int i = 0; i <= n; ++i)
+//             dp[0][i] = i;
+//         for (int i = 1; i <= m; ++i)
+//         {
+//             for (int j = 1; j <= n; ++j)
+//             {
+//                 if (word1[i - 1] == word2[j - 1])
+//                     dp[i][j] = dp[i - 1][j - 1];
+//                 else
+//                     dp[i][j] = min({dp[i - 1][j - 1], dp[i - 1][j], dp[i][j - 1]}) + 1;
+//             }
+//         }
+//         return dp[m][n];
+//     }
+// };
