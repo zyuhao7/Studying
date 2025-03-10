6657bd24b740a36427d42e09c822c42dc1b915c4
徐浩 <117811771+zyuhao7@users.noreply.github.com>
2025-03-10 21:56:31 +0800

    1441. 用栈操作构建数组   844. 比较含退格的字符串  682. 棒球比赛

---
+++ Algorithm/LeetCode题解/数据结构/Stack.cc
@@ -0,0 +1,121 @@
+#include <iostream>
+using namespace std;
+
+// day-2025-3-10
+// §3.1 基础
+// 1441. 用栈操作构建数组
+// class Solution {
+//     public:
+//         vector<string> buildArray(vector<int>& target, int n) {
+//             stack<int> st;
+//             vector<string> res;
+//             st.push(target[0]);
+//             int x = 1;
+//             for(int i =1; i <= target.size(); ++i)
+//             {
+//                 while(st.top() != x)
+//                 {
+//                     x++;
+//                     res.push_back("Push");
+//                     res.push_back("Pop");
+//                 }
+//                 res.push_back("Push");
+//                 x++;
+//                 if(i < target.size())
+//                   st.push(target[i]);
+//             }
+//             return res;
+//         }
+//     };
+
+// 844. 比较含退格的字符串
+// class Solution
+// {
+// public:
+//     bool backspaceCompare(string s, string t)
+//     {
+//         stack<char> st1, st2;
+//         for (auto c : s)
+//         {
+//             if (c == '#' && !st1.empty())
+//             {
+//                 st1.pop();
+//             }
+//             else if (c == '#' && st1.empty())
+//                 continue;
+//             else
+//                 st1.push(c);
+//         }
+//         for (auto c : t)
+//         {
+//             if (c == '#' && !st2.empty())
+//             {
+//                 st2.pop();
+//             }
+//             else if (c == '#' && st2.empty())
+//                 continue;
+//             else
+//                 st2.push(c);
+//         }
+//         while (!st1.empty() && !st2.empty())
+//         {
+//             auto t1 = st1.top(), t2 = st2.top();
+//             st1.pop();
+//             st2.pop();
+//             if (t1 != t2)
+//                 return false;
+//         }
+//         if (!st1.empty() || !st2.empty())
+//             return false;
+//         return true;
+//     }
+// };
+
+// 682. 棒球比赛
+// class Solution
+// {
+// public:
+//     int calPoints(vector<string> &ops)
+//     {
+//         stack<string> st;
+//         st.push(ops[0]);
+//         int n = ops.size();
+//         for (int i = 1; i < n; ++i)
+//         {
+//             if (ops[i] == "+")
+//             {
+//                 int n1 = stoi(st.top());
+//                 st.pop();
+//                 int n2 = stoi(st.top());
+//                 st.pop();
+//                 st.push(to_string(n2));
+//                 st.push(to_string(n1));
+//                 st.push(to_string(n1 + n2));
+//             }
+//             else if (ops[i] == "D")
+//             {
+//                 int n1 = stoi(st.top());
+//                 st.push(to_string(n1 * 2));
+//             }
+//             else if (ops[i] == "C")
+//             {
+//                 st.pop();
+//             }
+//             else
+//                 st.push(ops[i]);
+//         }
+
+//         int ans = 0;
+//         while (!st.empty())
+//         {
+//             ans += stoi(st.top());
+//             st.pop();
+//         }
+//         return ans;
+//     }
+// };
+
+int main()
+{
+    return 0;
+}
