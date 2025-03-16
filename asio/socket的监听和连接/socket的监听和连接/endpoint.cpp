a8e75b9b95f3be8deea3002ea07aa784333b1e91
徐浩 <117811771+zyuhao7@users.noreply.github.com>
2025-03-16 21:01:15 +0800

    socket的监听和连接

--- asio/socket的监听和连接/socket的监听和连接/endpoint.cpp
+++ asio/socket的监听和连接/socket的监听和连接/endpoint.cpp
@@ -29,10 +29,10 @@ int server_end_point()
 int create_tcp_socket()
 {
 	asio::io_context ioc;
-	asio::ip::tcp protocal = asio::ip::tcp::v4();
+	asio::ip::tcp protocol = asio::ip::tcp::v4();
 	asio::ip::tcp::socket sock(ioc);
 	boost::system::error_code ec;
-	sock.open(protocal, ec);
+	sock.open(protocol, ec);
 	if (ec.value() != 0)
 	{
 		std::cout << "Failed to open the socket. Error code = " << ec.value() << ". Message: " << ec.message();
@@ -44,15 +44,15 @@ int create_tcp_socket()
 int create_acceptor_socket()
 {
 	asio::io_context ioc;
-	//asio::ip::tcp::acceptor acceptor(ioc);
-	//asio::ip::tcp protocal = asio::ip::tcp::v4();
-	//boost::system::error_code ec;	
-	//acceptor.open(protocal, ec);
-	//if (ec.value() != 0)
+	// asio::ip::tcp protocol = asio::ip::tcp::v6();
+	// asio::ip::tcp::acceptor acceptor(ioc);
+	// boost::system::error_code ec;
+	// acceptor.open(protocol, ec);
+	// if (ec.value() != 0)
 	//{
 	//	std::cout << "Failed to open the acceptor socket. Error code = " << ec.value() << ". Message: " << ec.message();
 	//	return ec.value();
-	//}
+	// }
 	asio::ip::tcp::acceptor acceptor(ioc, asio::ip::tcp::endpoint(asio::ip::tcp::v4(), 3333));
 	return 0;
 }
@@ -77,13 +77,14 @@ int connect_to_end()
 {
 	std::string raw_ip_address = "127.0.0.1";
 	unsigned short port_num = 3333;
-	try {
+	try
+	{
 		asio::ip::tcp::endpoint ep(asio::ip::address::from_string(raw_ip_address), port_num);
 		asio::io_context ioc;
 		asio::ip::tcp::socket sock(ioc, ep.protocol());
 		sock.connect(ep);
 	}
-	catch (system::system_error& e)
+	catch (system::system_error &e)
 	{
 		std::cout << "Error occured! Error code = " << e.code() << ". Message: " << e.what();
 		return e.code().value();
@@ -100,12 +101,11 @@ int dns_connect_to_end()
 	asio::ip::tcp::resolver resolver(ioc);
 	try
 	{
-		asio::ip::tcp::resolver::iterator it =  resolver.resolve(query);
+		asio::ip::tcp::resolver::iterator it = resolver.resolve(query);
 		asio::ip::tcp::socket sock(ioc);
 		asio::connect(sock, it);
-
 	}
-	catch (system::system_error& e)
+	catch (system::system_error &e)
 	{
 		std::cout << "Error occured! Error code = " << e.code() << ". Message: " << e.what();
 		return e.code().value();
@@ -119,14 +119,15 @@ int accept_new_connection()
 	unsigned short port_num = 3333;
 	asio::ip::tcp::endpoint ep(asio::ip::address_v4::any(), port_num);
 	asio::io_context ioc;
-	try {
+	try
+	{
 		asio::ip::tcp::acceptor acceptor(ioc, ep.protocol());
 		acceptor.bind(ep);
 		acceptor.listen(BACKLOG_SIZE);
 		asio::ip::tcp::socket sock(ioc);
 		acceptor.accept(sock);
 	}
-	catch (system::system_error& e)
+	catch (system::system_error &e)
 	{
 		std::cout << "Error occured! Error code = " << e.code() << ". Message: " << e.what();
 		return e.code().value();
