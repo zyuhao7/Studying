494b4d7fa5972711df8383f7eb1dfa8c85a1bc05
徐浩 <117811771+zyuhao7@users.noreply.github.com>
2025-03-31 15:28:39 +0800

    链表快排

--- 数据结构/排序/Change_Sort.cc
+++ 数据结构/排序/Change_Sort.cc
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <list>
 #include <vector>
 using namespace std;
 
@@ -38,8 +39,44 @@ void quick_sort(vector<int> &arr, int l, int r)
     quick_sort(arr, l, j);
     quick_sort(arr, j + 1, r);
 }
+#include <algorithm>
+template <typename T>
+list<T> qsort_list(std::list<T> input)
+{
+    if (input.empty())
+        return input;
+    list<T> result;
+    result.splice(result.begin(), input, input.begin());
+
+    T const &pivot = *result.begin();
+    // 此时 dp 指向的是第一个大于等于 pivot 的位置
+    auto divide_point = std::partition(input.begin(), input.end(), [&](const &t)
+                                       { return t < pivot; });
+    list<T> lower_part;
+    lower_part.splice(lower_part.end(), input, input.begin(), divide_point);
+    auto new_lower(qsort_list(std::move(lower_part)));
+
+    auto new_higher(qsort_list(std::move(input)));
+    result.splice(result.end(), new_higher);
+    result.splice(result.begin(), new_lower);
+    return result;
+}
+
+void test_sequential_quick()
+{
+    std::list<int> numlists = {6, 1, 0, 7, 5, 2, 9, -1};
+    auto sort_result = qsort_list(numlists);
+    std::cout << "sorted result is ";
+    for (auto iter = sort_result.begin(); iter != sort_result.end(); iter++)
+    {
+        std::cout << " " << (*iter);
+    }
+    std::cout << std::endl;
+}
+
 int main()
 {
+    test_sequential_quick();
     vector<int> arr = {9, 5, 1, 4, 3};
     BubbleSort(arr);
     for (int i : arr)
