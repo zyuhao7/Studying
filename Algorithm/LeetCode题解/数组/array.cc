80064d03fd99ca0254d4c08c45582f9fddbdb339
徐浩 <117811771+zyuhao7@users.noreply.github.com>
2025-04-08 23:14:09 +0800

    LeetCode 1 两数之和

---
+++ Algorithm/LeetCode题解/数组/array.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+// day-2025-4-8 for interview
+// 1.两数之和
+// class Solution
+// {
+// public:
+//     vector<int> twoSum(vector<int> &nums, int target)
+//     {
+//         unordered_map<int, int> hash;
+//         for (int i = 0; i < nums.size(); ++i)
+//         {
+//             if (hash.count(target - nums[i]))
+//                 return {i, hash[target - nums[i]]};
+//             hash[nums[i]] = i;
+//         }
+//         return {};
+//     }
+// };
+
+int main()
+{
+}
