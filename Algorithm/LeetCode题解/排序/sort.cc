7457ec3ab6d51981121d80c8ec4386a993ff663c
徐浩 <117811771+zyuhao7@users.noreply.github.com>
2025-04-12 22:51:11 +0800

    148 链表排序 (归并思想)

--- Algorithm/LeetCode题解/排序/sort.cc
+++ Algorithm/LeetCode题解/排序/sort.cc
@@ -31,3 +31,54 @@
 //         return quick_sort(nums, 0, nums.size() - 1, nums.size() - k);
 //     }
 // };
+
+// 148 链表排序 (归并思想)
+// class Solution
+// {
+// public:
+//     ListNode *sortList(ListNode *head)
+//     {
+//         if (!head || !head->next)
+//             return head;
+//         ListNode *slow = head, *fast = head;
+//         while (fast->next && fast->next->next)
+//         {
+//             fast = fast->next->next;
+//             slow = slow->next;
+//         }
+//         ListNode *mid = slow->next;
+//         slow->next = nullptr;
+//         ListNode *left = sortList(head);
+//         ListNode *right = sortList(mid);
+//         return merge(left, right);
+//     }
+
+//     ListNode *merge(ListNode *l1, ListNode *l2)
+//     {
+//         if (!l1)
+//             return l2;
+//         if (!l2)
+//             return l1;
+//         ListNode *dummy = new ListNode(0);
+//         ListNode *cur = dummy;
+//         while (l1 && l2)
+//         {
+//             if (l1->val < l2->val)
+//             {
+//                 cur->next = l1;
+//                 l1 = l1->next;
+//             }
+//             else
+//             {
+//                 cur->next = l2;
+//                 l2 = l2->next;
+//             }
+//             cur = cur->next;
+//         }
+//         if (l1)
+//             cur->next = l1;
+//         if (l2)
+//             cur->next = l2;
+//         return dummy->next;
+//     }
+// };
