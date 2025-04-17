71f73c0418148968b5e3a93f1f72af7abe23094e
徐浩 <117811771+zyuhao7@users.noreply.github.com>
2025-04-17 22:38:14 +0800

    221  最大正方形

--- Algorithm/LeetCode题解/动规/dp.cc
+++ Algorithm/LeetCode题解/动规/dp.cc
@@ -915,3 +915,30 @@ using namespace std;
 //         return dp[m][n];
 //     }
 // };
+
+// 2025-4-17
+// 221  最大正方形
+// class Solution
+// {
+// public:
+//     int maximalSquare(vector<vector<char>> &matrix)
+//     {
+//         int m = matrix.size(), n = matrix[0].size();
+//         vector<vector<int>> dp(m, vector<int>(n));
+//         int ans = 0;
+//         for (int i = 0; i < m; ++i)
+//         {
+//             for (int j = 0; j < n; ++j)
+//             {
+//                 if (i == 0 || j == 0)
+//                     dp[i][j] = matrix[i][j] - '0';
+//                 else if (matrix[i][j] == '0')
+//                     dp[i][j] = 0;
+//                 else
+//                     dp[i][j] = min({dp[i][j - 1], dp[i - 1][j], dp[i - 1][j - 1]}) + 1;
+//                 ans = max(ans, dp[i][j] * dp[i][j]);
+//             }
+//         }
+//         return ans;
+//     }
+// };
