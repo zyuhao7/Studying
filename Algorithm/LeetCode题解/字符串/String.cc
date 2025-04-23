4e4254b00d3e501152e7274c86c4a10886a425df
徐浩 <117811771+zyuhao7@users.noreply.github.com>
2025-04-23 22:30:48 +0800

    5. 最长回文子串

--- Algorithm/LeetCode题解/字符串/String.cc
+++ Algorithm/LeetCode题解/字符串/String.cc
@@ -78,4 +78,41 @@ int countSubstrings(string s)
 }
 #endif
 
-// 未完待续···
+// day-2025-4-22
+// 5. 最长回文子串
+// 中心扩展法
+class Solution
+{
+public:
+    pair<int, int> expandAroundCenter(string s, int l, int r)
+    {
+        while (l >= 0 && r < s.size() && s[l] == s[r])
+        {
+            l--;
+            r++;
+        }
+        return {l + 1, r - 1};
+    }
+    string longestPalindrome(string s)
+    {
+        int st = 0, ed = 0;
+        for (int i = 0; i < s.size(); ++i)
+        {
+            auto [l1, r1] = expandAroundCenter(s, i, i);
+            auto [l2, r2] = expandAroundCenter(s, i, i + 1);
+            if (r1 - l1 > ed - st)
+            {
+                st = l1;
+                ed = r1;
+            }
+
+            if (r2 - l2 > ed - st)
+            {
+                st = l2;
+                ed = r2;
+            }
+        }
+        return s.substr(st, ed - st + 1);
+    }
+};
+
