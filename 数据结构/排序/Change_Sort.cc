015e2a2e05bbe08f4b50da09cc64746647ac5692
徐浩 <117811771+zyuhao7@users.noreply.github.com>
2025-08-03 10:22:16 +0800

    change_sort summary

--- 数据结构/排序/Change_Sort.cc
+++ 数据结构/排序/Change_Sort.cc
@@ -4,12 +4,23 @@
 using namespace std;
 
 // 冒泡排序
+/*
+     冒泡排序 (Bubble Sort)
+    - 原理 ：通过相邻元素比较和交换，逐步将最大元素"冒泡"到数组末尾
+    - 时间复杂度 ：O(n²)，最好情况O(n)（已排序）
+    - 空间复杂度 ：O(1)
+    - 稳定性 ：稳定
+    - 考点 ：
+        - 优化方式（如设置标志位提前结束）
+        - 理解稳定排序的概念
+        - 适用于小规模数据或几乎已排序的数据
+*/
 void BubbleSort(vector<int> &arr)
 {
     int n = arr.size();
-    for (int i = 0; i < n - 1; ++i)
+    for (int i = 0; i < n - 1; ++i) // 外层循环，控制冒泡的轮数
     {
-        for (int j = 0; j < n - i - 1; ++j)
+        for (int j = 0; j < n - i - 1; ++j) // 内层循环，控制每轮冒泡的次数
         {
             if (arr[j] > arr[j + 1])
             {
@@ -20,6 +31,19 @@ void BubbleSort(vector<int> &arr)
 }
 
 // 快速排序
+/*
+     快速排序 (Quick Sort)
+    - 原理 ：通过选择一个基准元素，将数组分为两个子数组，
+    一个子数组的所有元素都小于基准元素，另一个子数组的所有元素都大于基准元素。
+    然后递归地对两个子数组进行快速排序。
+    - 时间复杂度 ：O(nlogn)，最好情况O(nlogn)（已排序）
+    - 空间复杂度 ：O(logn)
+    - 稳定性 ：不稳定
+    - 考点 ：
+        - 优化方式（如三数取中法选择基准元素）
+        - 理解不稳定排序的概念
+        - 适用于大规模数据
+*/
 void quick_sort(vector<int> &arr, int l, int r)
 {
     if (l >= r)
@@ -29,10 +53,10 @@ void quick_sort(vector<int> &arr, int l, int r)
     {
         do
             ++i;
-        while (arr[i] < x);
+        while (arr[i] < x); // 找到第一个大于等于 x 的元素
         do
             --j;
-        while (arr[j] > x);
+        while (arr[j] > x); // 找到第一个小于等于 x 的元素
         if (i < j)
             swap(arr[i], arr[j]);
     }
@@ -40,25 +64,30 @@ void quick_sort(vector<int> &arr, int l, int r)
     quick_sort(arr, j + 1, r);
 }
 #include <algorithm>
+/*
+    链表的快速排序
+*/
 template <typename T>
 list<T> qsort_list(std::list<T> input)
 {
     if (input.empty())
         return input;
-    list<T> result;
-    result.splice(result.begin(), input, input.begin());
+    list<T> result;                                      // 用于存储排序后的结果
+    result.splice(result.begin(), input, input.begin()); // 取第一个元素作为基准元素
 
     T const &pivot = *result.begin();
-    // 此时 dp 指向的是第一个大于等于 pivot 的位置
+    // 此时 divide_point 指向的是第一个大于等于 pivot 的位置
     auto divide_point = std::partition(input.begin(), input.end(), [&](const &t)
                                        { return t < pivot; });
-    list<T> lower_part;
-    lower_part.splice(lower_part.end(), input, input.begin(), divide_point);
-    auto new_lower(qsort_list(std::move(lower_part)));
 
-    auto new_higher(qsort_list(std::move(input)));
-    result.splice(result.end(), new_higher);
-    result.splice(result.begin(), new_lower);
+    list<T> lower_part;                                                      // 用于存储小于 pivot 的元素
+    lower_part.splice(lower_part.end(), input, input.begin(), divide_point); // 将小于 pivot 的元素.splice 到 lower_part 中
+    auto new_lower(qsort_list(std::move(lower_part)));                       // 递归地对 lower_part 进行快速排序
+
+    auto new_higher(qsort_list(std::move(input))); // 递归地对 higher_part 进行快速排序
+    result.splice(result.end(), new_higher);       // 将 higher_part .splice 到 result 中
+
+    result.splice(result.begin(), new_lower); // 将 lower_part .splice 到 result 中
     return result;
 }
 
@@ -76,21 +105,21 @@ void test_sequential_quick()
 
 int main()
 {
-    test_sequential_quick();
-    vector<int> arr = {9, 5, 1, 4, 3};
-    BubbleSort(arr);
-    for (int i : arr)
-    {
-        cout << i << " ";
-    }
-    cout << endl;
+    // test_sequential_quick();
+    // vector<int> arr = {9, 5, 1, 4, 3};
+    // BubbleSort(arr);
+    // for (int i : arr)
+    // {
+    //     cout << i << " ";
+    // }
+    // cout << endl;
 
-    vector<int> arr2 = {9, 5, 1, 4, 3};
-    quick_sort(arr2, 0, arr2.size() - 1);
-    for (int i : arr2)
-    {
-        cout << i << " ";
-    }
-    cout << endl;
+    // vector<int> arr2 = {9, 5, 1, 4, 3};
+    // quick_sort(arr2, 0, arr2.size() - 1);
+    // for (int i : arr2)
+    // {
+    //     cout << i << " ";
+    // }
+    // cout << endl;
     return 0;
 }
