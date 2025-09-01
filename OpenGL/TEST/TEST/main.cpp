54f8fb01da45db11a6f14be3855adc910a736516
徐浩 <117811771+zyuhao7@users.noreply.github.com>
2025-09-01 22:17:37 +0800

    LearnOpenGL-入门

---
+++ OpenGL/TEST/TEST/main.cpp
@@ -0,0 +1,75 @@
+﻿#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+using namespace std;
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, 800, 600);
+}
+
+void processInput(GLFWwindow* window)
+{
+	if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS)
+	{
+		glfwSetWindowShouldClose(window, true);
+	}
+}
+
+int main()
+{
+	glfwInit();
+	// 设置主要和次要版本 
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3.3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3.3);
+
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	// 创建窗口对象
+	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+	if (window == NULL)
+	{
+		cout << "Filed to create GLFW window" << endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		cout << "Failed to init GLAD" << endl;
+		return -1;
+	}
+
+	// 设置视口
+	glViewport(0, 0, 800, 600);
+
+	// 注册窗口变化监视
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	// 定义顶点数组
+	float vertices[] = {
+		 -0.5f, -0.5f, 0.0f,
+	 0.5f, -0.5f, 0.0f,
+	 0.0f,  0.5f, 0.0f
+	};
+
+	// 生成 VBO 顶点缓冲对象
+	unsigned int VBO;
+	glGenBuffers(1, &VBO);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		processInput(window);
+
+		// 渲染指令
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	return 0;
+}
