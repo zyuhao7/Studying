ea5790c66769810cf22ca2480394ed6feac20785
徐浩 <117811771+zyuhao7@users.noreply.github.com>
2025-09-05 21:56:44 +0800

    纹理练习

---
+++ LearningOpenGL/src/08_texture_exercise/main.d
@@ -0,0 +1,3 @@
+src/08_texture_exercise/main.o: src/08_texture_exercise/main.cpp \
+ include/glad/glad.h include/KHR/khrplatform.h include/GLFW/glfw3.h \
+ include/tools/shader.h include/tools/stb_image.h
