278dd422ed8cca218ae39927c31a66a3b4582f82
徐浩 <117811771+zyuhao7@users.noreply.github.com>
2025-10-04 21:37:04 +0800

    D3Ddemo11

---
+++ Games/D3Ddemo11/D3DUtil.h
@@ -0,0 +1,14 @@
+#pragma once
+
+
+#ifndef HR
+#define HR(x)    { hr = x; if( FAILED(hr) ) { return hr; } }         //自定义一个HR宏，方便执行错误的返回
+#endif
+
+#ifndef SAFE_DELETE					
+#define SAFE_DELETE(p)       { if(p) { delete (p);     (p)=NULL; } }       //自定义一个SAFE_RELEASE()宏,便于指针资源的释放
+#endif    
+
+#ifndef SAFE_RELEASE			
+#define SAFE_RELEASE(p)      { if(p) { (p)->Release(); (p)=NULL; } }     //自定义一个SAFE_RELEASE()宏,便于COM资源的释放
+#endif
